930f74b0588884d717fde169e0347daf8d0b0cd4
Jae Sung Park <alberto.park@gmail.com>
2018-01-29 14:21:58 +0900

    fix(interaction): Correct mouse event create (#252)
    - Use MouseEvent to create whenever is supported.
    - Added params description for .tooltip.show()
    
    Fix #251
    Close #252

--- src/api/api.tooltip.js
+++ src/api/api.tooltip.js
@@ -14,9 +14,16 @@ const tooltip = extend(() => {}, {
 	 * @method tooltip․show
 	 * @instance
 	 * @memberOf Chart
-	 * @param {Array} args
+	 * @param {Object} args The object can consist with following members:<br>
+	 *
+	 *    | Key | Type | Description |
+	 *    | --- | --- | --- |
+	 *    | data | Object | Determine focus data with following keys: `x`, `id`, `value` and `index` |
+	 *    | index | Number | Determine focus by index |
+	 *    | mouse | Array | Determine x and y coordinate value |
+	 *    | x | Number | Determine focus by x Axis index |
 	 */
-	show: function(args) {
+	show: function(args = {}) {
 		const $$ = this.internal;
 		let index;
 		let mouse;
@@ -47,8 +54,9 @@ const tooltip = extend(() => {}, {
 		}
 
 		// emulate mouse events to show
-		$$.dispatchEvent("mouseover", index, mouse);
-		$$.dispatchEvent("mousemove", index, mouse);
+		["mouseover", "mousemove"].forEach(eventName => {
+			$$.dispatchEvent(eventName, index, mouse);
+		});
 
 		$$.config.tooltip_onshow.call($$, args.data);
 	},
