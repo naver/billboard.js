c34893e0bcfa4c076fecce58509597f45216e9a5
Jae Sung Park <alberto.park@gmail.com>
2018-01-29 18:59:44 +0900

    test(tooltip): Add tooltip's API test codes
    Ref #248

--- spec/api/api.tooltip-spec.js
+++ spec/api/api.tooltip-spec.js
@@ -6,25 +6,72 @@
 import util from "../assets/util";
 import CLASS from "../../src/config/classes";
 
-// @TODO
-describe.skip("API tooltip", () => {
+describe("API tooltip", () => {
 	const chart = util.generate({
 		data: {
 			x: "x",
 			columns: [
-				["x", "a", "b", "c", "d", "e"],
+				["x", "2018-01-01", "2018-01-02", "2018-01-03", "2018-01-04", "2018-01-05"],
 				["data1", 30, 200, 100, 400, 150],
 				["data2", 5000, 2000, 1000, 4000, 1500]
 			]
 		},
 		axis: {
 			x: {
-				type: "category"
+				type: "timeseries",
+				tick: {
+					format: "%Y-%m-%d"
+				}
 			}
 		}
 	});
 
-	it("", () => {
+	it("should show/hide tooltip using 'index' option", () => {
+		const internal = chart.internal;
+		const config = internal.config;
+		const tooltip = internal.tooltip;
+		const x = 1;
 
+		// show tooltip
+		chart.tooltip.show({
+			index: x,
+			mouse: [10, 200]
+		});
+
+		expect(tooltip.html()).to.be.ok;
+		expect(tooltip.style("display")).to.be.equal("block");
+
+		// check if tooltip data are correctly rendering
+		chart.data().forEach(v => {
+			const id = v.id;
+			const data = tooltip.select(`.${CLASS.tooltipName}-${id}`);
+			const value = chart.data(id)[0].values[x];
+
+			expect(id).to.be.equal(v.id);
+			expect(tooltip.select("th").html()).to.be.equal(d3.timeFormat(config.axis_x_tick_format)(value.x));
+			expect(data.select(".name").text()).to.be.equal(id);
+			expect(+data.select(".value").html()).to.be.equal(value.value);
+		});
+
+		// hide tooltip
+		chart.tooltip.hide();
+
+		expect(tooltip.style("display")).to.be.equal("none");
+	});
+
+	it("should show/hide tooltip using 'x' option", () => {
+		const tooltip = chart.internal.tooltip;
+		const x = new Date("2018-01-03 00:00");
+
+		// show tooltip
+		chart.tooltip.show({ x });
+
+		expect(tooltip.html()).to.be.ok;
+		expect(tooltip.style("display")).to.be.equal("block");
+
+		// hide tooltip
+		chart.tooltip.hide();
+
+		expect(tooltip.style("display")).to.be.equal("none");
 	});
 });
