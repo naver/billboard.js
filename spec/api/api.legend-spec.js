295b740177c6516e943c2d427d5778a0c1a75ccc
Jae Sung Park <alberto.park@gmail.com>
2018-02-01 15:50:12 +0900

    test(legend): Add test code for .legend.show/hide()
    Fix #249
    Close #268

--- spec/api/api.legend-spec.js
+++ spec/api/api.legend-spec.js
@@ -6,8 +6,7 @@
 import util from "../assets/util";
 import CLASS from "../../src/config/classes";
 
-// @TODO
-describe.skip("API legend", () => {
+describe("API legend", () => {
 	const chart = util.generate({
 		data: {
 			x: "x",
@@ -24,7 +23,45 @@ describe.skip("API legend", () => {
 		}
 	});
 
-	it("", () => {
+	it("it should hide all legends", () => {
+		chart.legend.hide();
 
+		chart.internal.svg.selectAll(`.${CLASS.legendItem}`).each(function() {
+			expect(+this.style.opacity).to.be.equal(0);
+		});
+	});
+
+	it("it should show all hide legends", done => {
+		chart.legend.show();
+
+		setTimeout(() => {
+			chart.internal.svg.selectAll(`.${CLASS.legendItem}`).each(function() {
+
+				expect(+this.style.opacity).to.be.equal(1);
+			});
+
+			done();
+		}, 500)
+	});
+
+	it("it should hide 'data1' legend", () => {
+		chart.legend.hide("data1");
+
+		chart.internal.svg.selectAll(`.${CLASS.legendItem}`).each(function(v) {
+			expect(+this.style.opacity).to.be.equal(v === "data1" ? 0 : 1);
+		});
+	});
+
+
+	it("it should show 'data1' legend", done => {
+		chart.legend.show("data1");
+
+		setTimeout(() => {
+			chart.internal.svg.selectAll(`.${CLASS.legendItem}`).each(function(v) {
+				expect(+this.style.opacity).to.be.equal(1);
+			});
+
+			done();
+		}, 500);
 	});
 });
