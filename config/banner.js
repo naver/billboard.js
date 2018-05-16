83102ea3e2ebd2ebafee28d897544f20eea867d4
Jae Sung Park <alberto.park@gmail.com>
2018-05-16 11:54:37 +0900

    skip: update banner to use template literals - Add version info for packaged build [skip ci]

--- config/banner.js
+++ config/banner.js
@@ -1,19 +1,19 @@
-var pkg = require("../package.json");
+const pkg = require("../package.json");
 
 module.exports = {
-	"production": [
-			"Copyright (c) 2017 " + pkg.author,
-			pkg.name + " project is licensed under the " + pkg.license + " license",
-			"",
-			pkg.name + ", JavaScript chart library",
-			pkg.homepage,
-			"",
-			"@version " + pkg.version
-		].join("\r\n"),
-	"packaged": [
-			"",
-			"",
-			"All-in-one packaged file for ease use of '" + pkg.name + "' with below dependency.",
-			"- " + Object.keys(pkg.dependencies).join(", ")
-		].join("\r\n")
+	production: [
+		`Copyright (c) 2017 ${pkg.author}`,
+		`${pkg.name} project is licensed under the ${pkg.license} license`,
+		"",
+		`${pkg.name}, JavaScript chart library`,
+		pkg.homepage,
+		"",
+		`@version ${pkg.version}`
+	].join("\r\n"),
+	packaged: [
+		"",
+		"",
+		`All-in-one packaged file for ease use of '${pkg.name}' with below dependency.`,
+		`- ${Object.entries(pkg.dependencies).map(v => v.join(" ")).join(", ")}`
+	].join("\r\n")
 };
