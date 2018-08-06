naver/billboard.js

commit c0f1212854b35b45133b771bd5415d7aede418dd

refactor(all): Removed & updated unnecessary codes (#527)
- Remove the use of MutationObserver
- Add call function utility function

Fix #524
Close #527

src/internals/ChartInternal.js
@@ -18,7 +18,7 @@ import {transition as d3Transition} from "d3-transition";
 
 import Axis from "../axis/Axis";
 import CLASS from "../config/classes";
-import {notEmpty, asHalfPixel, getOption, isValue, isArray, isFunction, isDefined, isUndefined, isString, isNumber, isObject} from "./util";
+import {notEmpty, asHalfPixel, getOption, isValue, isArray, isFunction, isDefined, isString, isNumber, isObject, callFn} from "./util";
 
 /**
  * Internal chart class.
@@ -43,8 +43,7 @@ export default class ChartInternal {
 		const config = $$.config;
 
 		// can do something
-
-		isFunction(config.onbeforeinit) && config.onbeforeinit.call($$);
+		callFn(config.onbeforeinit, $$);
 	}
 
 	afterInit() {
@@ -52,8 +51,7 @@ export default class ChartInternal {
 		const config = $$.config;
 
 		// can do something
-
-		isFunction(config.onafterinit) && config.onafterinit.call($$);
+		callFn(config.onafterinit, $$);
 	}
 
 	init() {
@@ -168,8 +166,6 @@ export default class ChartInternal {
 		$$.rotated_padding_top = 5;
 
 		$$.withoutFadeIn = {};
-		$$.intervalForObserveInserted = undefined;
-
 		$$.inputType = $$.convertInputType();
 
 		$$.axes.subx = d3SelectAll([]); // needs when excluding subchart.js
@@ -178,7 +174,6 @@ export default class ChartInternal {
 	initWithData(data) {
 		const $$ = this;
 		const config = $$.config;
-		let binding = true;
 
 		$$.axis = new Axis($$);
 
@@ -202,12 +197,6 @@ export default class ChartInternal {
 		// select bind element
 		$$.selectChart = isFunction(bindto.element.node) ?
 			config.bindto.element : d3Select(!bindto.element ? [] : bindto.element);
-
-		if ($$.selectChart.empty()) {
-			$$.selectChart = d3Select(document.createElement("div")).style("opacity", "0");
-			$$.observeInserted($$.selectChart);
-			binding = false;
-		}
 
 		$$.selectChart.html("").classed(bindto.classname, true);
 
@@ -254,7 +243,6 @@ export default class ChartInternal {
 		$$.orgXDomain = $$.x.domain();
 
 		// -- Basic Elements --
-
 		$$.svg = $$.selectChart.append("svg")
 			.style("overflow", "hidden")
 			.style("display", "block");
@@ -263,8 +251,8 @@ export default class ChartInternal {
 			const isTouch = $$.inputType === "touch";
 
 			$$.svg
-				.on(isTouch ? "touchstart" : "mouseenter", () => config.onover.call($$))
-				.on(isTouch ? "touchend" : "mouseleave", () => config.onout.call($$));
+				.on(isTouch ? "touchstart" : "mouseenter", () => callFn(config.onover, $$))
+				.on(isTouch ? "touchend" : "mouseleave", () => callFn(config.onout, $$));
 		}
 
 		config.svg_classname &&
@@ -285,10 +273,6 @@ export default class ChartInternal {
 		}
 
 		$$.updateSvgSize();
-
-		// Set initialized scales to brush and zoom
-		// if ($$.brush) { $$.brush.scale($$.subX); }
-		// if (config.zoom_enabled === true || config.zoom_enabled_type) { $$.zoom.scale($$.x); }
 
 		// Define regions
 		const main = $$.svg.append("g").attr("transform", $$.getTranslate("main"));
@@ -355,28 +339,26 @@ export default class ChartInternal {
 		$$.updateTargets($$.data.targets);
 
 		// Draw with targets
-		if (binding) {
-			$$.updateDimension();
+		$$.updateDimension();
 
-			// oninit callback
-			config.oninit.call($$);
+		// oninit callback
+		config.oninit.call($$);
 
-			$$.redraw({
-				withTransition: false,
-				withTransform: true,
-				withUpdateXDomain: true,
-				withUpdateOrgXDomain: true,
-				withTransitionForAxis: false,
-				initializing: true
-			});
+		$$.redraw({
+			withTransition: false,
+			withTransform: true,
+			withUpdateXDomain: true,
+			withUpdateOrgXDomain: true,
+			withTransitionForAxis: false,
+			initializing: true
+		});
 
-			// data.onmin/max callback
-			if (config.data_onmin || config.data_onmax) {
-				const minMax = $$.getMinMaxData();
+		// data.onmin/max callback
+		if (config.data_onmin || config.data_onmax) {
+			const minMax = $$.getMinMaxData();
 
-				isFunction(config.data_onmin) && config.data_onmin.call($$, minMax.min);
-				isFunction(config.data_onmax) && config.data_onmax.call($$, minMax.max);
-			}
+			callFn(config.data_onmin, $$, minMax.min);
+			callFn(config.data_onmax, $$, minMax.max);
 		}
 
 		// Bind resize event
@@ -483,9 +465,8 @@ export default class ChartInternal {
 			left: 0
 		};
 
-		if ($$.updateSizeForLegend) {
+		$$.updateSizeForLegend &&
 			$$.updateSizeForLegend(legendHeight, legendWidth);
-		}
 
 		$$.width = $$.currentWidth - $$.margin.left - $$.margin.right;
 		$$.height = $$.currentHeight - $$.margin.top - $$.margin.bottom;
@@ -504,8 +485,13 @@ export default class ChartInternal {
 		$$.height2 = isRotated ?
 			$$.height : $$.currentHeight - $$.margin2.top - $$.margin2.bottom;
 
-		if ($$.width2 < 0) { $$.width2 = 0; }
-		if ($$.height2 < 0) { $$.height2 = 0; }
+		if ($$.width2 < 0) {
+			$$.width2 = 0;
+		}
+
+		if ($$.height2 < 0) {
+			$$.height2 = 0;
+		}
 
 		// for arc
 		$$.arcWidth = $$.width - ($$.isLegendRight ? legendWidth + 10 : 0);
@@ -1128,54 +1114,6 @@ export default class ChartInternal {
 
 		$$.updateSvgSize();
 		$$.transformAll(false);
-	}
-
-	observeInserted(selection) {
-		const $$ = this;
-
-		if (isUndefined(MutationObserver)) {
-			console && console.error &&
-				console.error("MutationObserver not defined.");
-
-			return;
-		}
-
-		const observer = new MutationObserver(mutations => {
-			mutations.forEach(mutation => {
-				if (mutation.type === "childList" && mutation.previousSibling) {
-					observer.disconnect();
-
-					// need to wait for completion of load because size calculation requires the actual sizes determined after that completion
-					$$.intervalForObserveInserted = window.setInterval(() => {
-						// parentNode will NOT be null when completed
-						if (selection.node().parentNode) {
-							window.clearInterval($$.intervalForObserveInserted);
-							$$.updateDimension();
-
-							$$.brush && $$.brush.update();
-							$$.config.oninit.call($$);
-
-							$$.redraw({
-								withTransform: true,
-								withUpdateXDomain: true,
-								withUpdateOrgXDomain: true,
-								withTransition: false,
-								withTransitionForTransform: false,
-								withLegend: true
-							});
-
-							selection.transition().style("opacity", "1");
-						}
-					}, 10);
-				}
-			});
-		});
-
-		observer.observe(selection.node(), {
-			attributes: true,
-			childList: true,
-			characterData: true
-		});
 	}
 
 	bindResize() {

src/api/api.chart.js
@@ -64,7 +64,6 @@ extend(Chart.prototype, {
 			$$.charts.splice($$.charts.indexOf(this), 1);
 
 			// clear timers
-			isDefined($$.intervalForObserveInserted) && window.clearInterval($$.intervalForObserveInserted);
 			isDefined($$.resizeTimeout) && window.clearTimeout($$.resizeTimeout);
 
 			d3Select(window).on("resize.bb", null);

src/api/api.load.js
@@ -3,7 +3,7 @@
  * billboard.js project is licensed under the MIT license
  */
 import Chart from "../internals/Chart";
-import {extend, isString, isBoolean} from "../internals/util";
+import {extend, isString, isArray, isBoolean} from "../internals/util";
 
 extend(Chart.prototype, {
 	/**
@@ -110,7 +110,7 @@ extend(Chart.prototype, {
 		const $$ = this.internal;
 		let args = argsValue || {};
 
-		if (args instanceof Array) {
+		if (isArray(args)) {
 			args = {ids: args};
 		} else if (isString(args)) {
 			args = {ids: [args]};

src/api/api.zoom.js
@@ -8,7 +8,7 @@ import {
 } from "d3-array";
 import {zoomIdentity as d3ZoomIdentity} from "d3-zoom";
 import Chart from "../internals/Chart";
-import {isDefined, isObject, isFunction, isString, extend} from "../internals/util";
+import {callFn, isDefined, isObject, isString, extend} from "../internals/util";
 
 /**
  * Zoom by giving x domain.
@@ -62,9 +62,7 @@ const zoom = function(domainValue) {
 		});
 
 		$$.setZoomResetButton();
-
-		isFunction($$.config.zoom_onzoom) &&
-			$$.config.zoom_onzoom.call(this, $$.x.orgDomain());
+		callFn($$.config.zoom_onzoom, this, $$.x.orgDomain());
 	} else {
 		resultDomain = $$.zoomScale ?
 			$$.zoomScale.domain() : $$.x.orgDomain();

src/axis/bb.axis.js
@@ -495,9 +495,7 @@ export default function(params = {}) {
 
 	axis.tickValues = function(x) {
 		if (isFunction(x)) {
-			tickValues = function() {
-				return x(scale.domain());
-			};
+			tickValues = () => x(scale.domain());
 		} else {
 			if (!arguments.length) {
 				return tickValues;

src/config/Options.js
@@ -15,10 +15,6 @@ export default class Options {
 			 * Specify the CSS selector or the element which the chart will be set to. D3 selection object can be specified also.
 			 * If other chart is set already, it will be replaced with the new one (only one chart can be set in one element).<br><br>
 			 * If this option is not specified, the chart will be generated but not be set. Instead, we can access the element by chart.element and set it by ourselves.<br>
-			 * - **NOTE:**
-			 *  > When chart is not bound, it'll start observing if `chart.element` is bound by MutationObserver.<br>
-			 *  > In this case, polyfill is required in IE9 and IE10 because they do not support MutationObserver.<br>
-			 *  > On the other hand, if chart always will be bound, polyfill will not be required because MutationObserver will never be called.
 			 * @name bindto
 			 * @memberOf Options
 			 * @property {String|HTMLElement|d3.selection} bindto=#chart Specify the element where chart will be drawn.

src/interactions/zoom.js
@@ -14,7 +14,7 @@ import {drag as d3Drag} from "d3-drag";
 import {zoom as d3Zoom} from "d3-zoom";
 import ChartInternal from "../internals/ChartInternal";
 import CLASS from "../config/classes";
-import {extend, diffDomain, isFunction} from "../internals/util";
+import {extend, callFn, diffDomain} from "../internals/util";
 
 extend(ChartInternal.prototype, {
 	/**
@@ -109,13 +109,12 @@ extend(ChartInternal.prototype, {
 	onZoomStart() {
 		const $$ = this;
 		const event = d3Event.sourceEvent;
-		const onzoomstart = $$.config.zoom_onzoomstart;
 
 		$$.zoom.altDomain = event.altKey ?
 			$$.x.orgDomain() : null;
 
 		$$.zoom.startEvent = event;
-		isFunction(onzoomstart) && onzoomstart.call($$.api, event);
+		callFn($$.config.zoom_onzoomstart, $$.api, event);
 	},
 
 	/**
@@ -159,7 +158,7 @@ extend(ChartInternal.prototype, {
 		});
 
 		$$.cancelClick = isMousemove;
-		isFunction(config.zoom_onzoom) && config.zoom_onzoom.call($$.api, $$.x.orgDomain());
+		callFn(config.zoom_onzoom, $$.api, $$.x.orgDomain());
 	},
 
 	/**
@@ -168,7 +167,6 @@ extend(ChartInternal.prototype, {
 	 */
 	onZoomEnd() {
 		const $$ = this;
-		const onzoomend = $$.config.zoom_onzoomend;
 		const startEvent = $$.zoom.startEvent;
 
 		// if click, do nothing. otherwise, click interaction will be canceled.
@@ -179,7 +177,7 @@ extend(ChartInternal.prototype, {
 		$$.redrawEventRect();
 		$$.updateZoom();
 
-		isFunction(onzoomend) && onzoomend.call($$.api, $$.x.orgDomain());
+		callFn($$.config.zoom_onzoomend, $$.api, $$.x.orgDomain());
 	},
 
 	/**

src/internals/class.js
@@ -41,6 +41,7 @@ extend(ChartInternal.prototype, {
 			if (ids.indexOf(id) < 0) {
 				ids.push(id);
 			}
+
 			return classes[ids.indexOf(id) % classes.length];
 		};
 	},

commit 7300e6543f2abb1085298194e9e58f4200b5856c

skip: Correction on referring subchart prop name
This was caused by the #527
[skip ci]

src/internals/ChartInternal.js
@@ -557,7 +557,7 @@ export default class ChartInternal {
 	getWithOption(options) {
 		const withOptions = {
 			Y: true,
-			SubChart: true,
+			Subchart: true,
 			Transition: true,
 			EventRect: true,
 			Dimension: true,
@@ -748,17 +748,16 @@ export default class ChartInternal {
 		hasRadar && $$.redrawRadar();
 
 		// subchart
-		config.subchart_show &&
-			$$.redrawSubchart &&
-				$$.redrawSubchart(
-					wth.Subchart,
-					transitions,
-					duration,
-					durationForExit,
-					areaIndices,
-					barIndices,
-					lineIndices
-				);
+		config.subchart_show && $$.redrawSubchart &&
+			$$.redrawSubchart(
+				wth.Subchart,
+				transitions,
+				duration,
+				durationForExit,
+				areaIndices,
+				barIndices,
+				lineIndices
+			);
 
 		// circles for select
 		main.selectAll(`.${CLASS.selectedCircles}`)