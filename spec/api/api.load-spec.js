naver/billboard.js

commit f3881bc46b8ff4532fdabb3dac77bb3bae199c39

fix(api): Fix loading to maintain correct x index
- Corrected to point correct x axis index value when the loaded x is already exists
- Fix on getting bubble radius when is used with area-range type

Fix #517
Close #519

src/data/data.convert.js
@@ -192,6 +192,7 @@ extend(ChartInternal.prototype, {
 		const config = $$.config;
 		const ids = d3Keys(data[0]).filter($$.isNotX, $$);
 		const xs = d3Keys(data[0]).filter($$.isX, $$);
+		let xsData;
 
 		// save x for update data by load when custom x and bb.x API
 		ids.forEach(id => {
@@ -200,29 +201,30 @@ extend(ChartInternal.prototype, {
 			if (this.isCustomX() || this.isTimeSeries()) {
 				// if included in input data
 				if (xs.indexOf(xKey) >= 0) {
-					this.data.xs[id] =
-						(appendXs && $$.data.xs[id] ? $$.data.xs[id] : [])
-							.concat(
-								data.map(d => d[xKey])
-									.filter(isValue)
-									.map((rawX, i) => $$.generateTargetX(rawX, id, i))
-							);
+					xsData = ((appendXs && $$.data.xs[id]) || [])
+						.concat(
+							data.map(d => d[xKey])
+								.filter(isValue)
+								.map((rawX, i) => $$.generateTargetX(rawX, id, i))
+						);
 				} else if (config.data_x) {
 					// if not included in input data, find from preloaded data of other id's x
-					this.data.xs[id] = this.getOtherTargetXs();
+					xsData = this.getOtherTargetXs();
 				} else if (notEmpty(config.data_xs)) {
 					// if not included in input data, find from preloaded data
-					$$.data.xs[id] = $$.getXValuesOfXKey(xKey, $$.data.targets);
+					xsData = $$.getXValuesOfXKey(xKey, $$.data.targets);
 				}
 				// MEMO: if no x included, use same x of current will be used
 			} else {
-				$$.data.xs[id] = data.map((d, i) => i);
+				xsData = data.map((d, i) => i);
 			}
+
+			xsData && (this.data.xs[id] = xsData);
 		});
 
 		// check x is defined
 		ids.forEach(id => {
-			if (!$$.data.xs[id]) {
+			if (!xsData) {
 				throw new Error(`x is not defined for id = "${id}".`);
 			}
 		});
@@ -230,21 +232,25 @@ extend(ChartInternal.prototype, {
 		// convert to target
 		const targets = ids.map((id, index) => {
 			const convertedId = config.data_idConverter(id);
+			const xKey = $$.getXKey(id);
+			const isCategorized = $$.isCustomX() && $$.isCategorized();
+			const hasCategory = isCategorized && data.map(v => v.x)
+				.every(v => config.axis_x_categories.indexOf(v) > -1);
 
 			return {
 				id: convertedId,
 				id_org: id,
 				values: data.map((d, i) => {
-					const xKey = $$.getXKey(id);
 					const rawX = d[xKey];
-					const value = d[id] !== null && !isNaN(d[id]) ?
-						+d[id] : (isArray(d[id]) || ($$.isObject(d[id]) && d[id].high) ? d[id] : null);
-
+					let value = d[id];
 					let x;
 
+					value = value !== null && !isNaN(value) ?
+						+d[id] : (isArray(value) || ($$.isObject(value) && value.high) ? value : null);
+
 					// use x as categories if custom x and categorized
-					if ($$.isCustomX() && $$.isCategorized() && index === 0 && !isUndefined(rawX)) {
-						if (index === 0 && i === 0) {
+					if (isCategorized && index === 0 && !isUndefined(rawX)) {
+						if (!hasCategory && index === 0 && i === 0) {
 							config.axis_x_categories = [];
 						}
 
@@ -270,8 +276,6 @@ extend(ChartInternal.prototype, {
 
 		// finish targets
 		targets.forEach(t => {
-			let i;
-
 			// sort values by its x
 			if (config.data_xSort) {
 				t.values = t.values.sort((v1, v2) => {
@@ -283,10 +287,10 @@ extend(ChartInternal.prototype, {
 			}
 
 			// indexing each value
-			i = 0;
+			let i = 0;
 
 			t.values.forEach(v => {
-				v.index = i++;
+				v.index = $$.data.targets ? $$.getIndexByX(v.x) : i++;
 			});
 
 			// this needs to be sorted because its index and value.index is identical

spec/api/api.load-spec.js
@@ -14,9 +14,6 @@ describe("API load", function() {
 				["data1", 30, 200, 100, 400, 150, 250],
 				["data2", 5000, 2000, 1000, 4000, 1500, 2500]
 			]
-		},
-		point: {
-			show: true
 		}
 	};
 
@@ -32,20 +29,74 @@ describe("API load", function() {
 			chart.load({
 				columns: [
 					["data3", 800, 500, 900, 500, 1000, 700]
-				]
+				],
+				done: () => {
+					const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
+					const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
+					const circles = main.selectAll(`.${CLASS.circles}.${CLASS.circles}-data3 circle`);
+
+					expect(target.size()).to.be.equal(1);
+					expect(legendItem.size()).to.be.equal(1);
+					expect(circles.size()).to.be.equal(6);
+
+					done();
+				}
 			});
+		});
+	});
 
-			setTimeout(() => {
-				const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
-				const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
-				const circles = main.selectAll(`.${CLASS.circles}.${CLASS.circles}-data3 circle`);
+	describe("timeseries data as column", () => {
+		let date = ["2013-01-01", "2013-01-02", "2013-01-03", "2013-01-04", "2013-01-05", "2013-01-06"];
 
-				expect(target.size()).to.be.equal(1);
-				expect(legendItem.size()).to.be.equal(1);
-				expect(circles.size()).to.be.equal(6);
+		before(() => {
+			args = {
+				data: {
+					x: "x",
+					columns: [
+						["x"].concat(date),
+						["data1", 30, 200, 100, 400, 150, 250],
+						["data2", 5000, 2000, 1000, 4000, 1500, 2500]
+					]
+				},
+				axis: {
+					x: {
+						type: "timeseries",
+						tick: {
+							format: "%Y-%m-%d"
+						}
+					}
+				},
+			};
+		});
 
-				done();
-			}, 500);
+		it("should load additional data", done => {
+			const main = chart.internal.main;
+			const legend = chart.internal.legend;
+
+			chart.load({
+				columns: [
+					["x"].concat(date.concat().splice(1, 3)),
+					["data3", 400, 500, 450]
+				],
+				done: () => {
+					const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
+					const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
+					const circles = main.selectAll(`.${CLASS.circles}.${CLASS.circles}-data3 circle`);
+					const tickTexts = main.selectAll(`.${CLASS.axisX} g.tick text`);
+
+					expect(target.size()).to.be.equal(1);
+					expect(legendItem.size()).to.be.equal(1);
+					expect(circles.size()).to.be.equal(3);
+
+					tickTexts.each(function(d, i) {
+						const text = d3.select(this).select("tspan").text();
+
+						expect(text).to.be.equal(date[i]);
+					});
+
+					done();
+				}
+			});
 		});
 	});
 
@@ -64,44 +115,42 @@ describe("API load", function() {
 					x: {
 						type: "category"
 					}
-				},
-				point: {
-					show: false
 				}
 			};
 		});
 
 		describe("as column", () => {
-			it("should load additional data", done => {
+			it("should load additional data #1", done => {
 				const main = chart.internal.main;
 				const legend = chart.internal.legend;
 
 				chart.load({
 					columns: [
-						["data3", 800, 500, 900, 500, 1000, 700]
-					]
+						["x", "cat2", "cat3", "cat4"],
+						["data3", 800, 500, 900]
+					],
+					done: () => {
+						const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
+						const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
+						const tickTexts = main.selectAll(`.${CLASS.axisX} g.tick text`);
+						const expected = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6"];
+
+						expect(target.size()).to.be.equal(1);
+						expect(target.selectAll("circle").size()).to.be.equal(3);
+						expect(legendItem.size()).to.be.equal(1);
+
+						tickTexts.each(function(d, i) {
+							const text = d3.select(this).select("tspan").text();
+
+							expect(text).to.be.equal(expected[i]);
+						});
+
+						done();
+					}
 				});
-
-				setTimeout(() => {
-					const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
-					const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
-					const tickTexts = main.selectAll(`.${CLASS.axisX} g.tick text`);
-					const expected = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6"];
-
-					expect(target.size()).to.be.equal(1);
-					expect(legendItem.size()).to.be.equal(1);
-
-					tickTexts.each(function(d, i) {
-						const text = d3.select(this).select("tspan").text();
-
-						expect(text).to.be.equal(expected[i]);
-					});
-
-					done();
-				}, 500);
 			});
 
-			it("should load additional data", done => {
+			it("should load additional data #2", done => {
 				const main = chart.internal.main;
 				const legend = chart.internal.legend;
 
@@ -109,26 +158,25 @@ describe("API load", function() {
 					columns: [
 						["x", "new1", "new2", "new3", "new4", "new5", "new6"],
 						["data3", 800, 500, 900, 500, 1000, 700]
-					]
+					],
+					done: () => {
+						const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
+						const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
+						const tickTexts = main.selectAll(`.${CLASS.axisX} g.tick text`);
+						const expected = ["new1", "new2", "new3", "new4", "new5", "new6"];
+
+						expect(target.size()).to.be.equal(1);
+						expect(legendItem.size()).to.be.equal(1);
+
+						tickTexts.each(function(d, i) {
+							const text = d3.select(this).select("tspan").text();
+
+							expect(text).to.be.equal(expected[i]);
+						});
+
+						done();
+					}
 				});
-
-				setTimeout(() => {
-					const target = main.select(`.${CLASS.chartLine}.${CLASS.target}.${CLASS.target}-data3`);
-					const legendItem = legend.select(`.${CLASS.legendItem}.${CLASS.legendItem}-data3`);
-					const tickTexts = main.selectAll(`.${CLASS.axisX} g.tick text`);
-					const expected = ["new1", "new2", "new3", "new4", "new5", "new6"];
-
-					expect(target.size()).to.be.equal(1);
-					expect(legendItem.size()).to.be.equal(1);
-
-					tickTexts.each(function(d, i) {
-						const text = d3.select(this).select("tspan").text();
-
-						expect(text).to.be.equal(expected[i]);
-					});
-
-					done();
-				}, 500);
 			});
 		});
 	});
@@ -157,21 +205,22 @@ describe("API load", function() {
 				{name: "www.site8.com", upload: 100, download: 500}
 			];
 
-			chart.load({json});
+			chart.load({
+				json,
+				done: () => {
+					const categories = chart.categories();
+					const upload = chart.data.values("upload");
+					const download = chart.data.values("download");
 
-			setTimeout(() => {
-				const categories = chart.categories();
-				const upload = chart.data.values("upload");
-				const download = chart.data.values("download");
+					json.forEach((v, i) => {
+						expect(v.name).to.be.equal(categories[i]);
+						expect(v.upload).to.be.equal(upload[i]);
+						expect(v.download).to.be.equal(download[i]);
+					});
 
-				json.forEach((v, i) => {
-					expect(v.name).to.be.equal(categories[i]);
-					expect(v.upload).to.be.equal(upload[i]);
-					expect(v.download).to.be.equal(download[i]);
-				});
-
-				done();
-			}, 1000);
+					done();
+				}
+			});
 		});
 	});
 });

commit 1f30775d23a9fd2e19418bae2a299509a1b0d673

fix(axis): Correct axis label position
- Correct .getMaxTickWidth() to get last max axis width value
- Some refactorings
- Fix side effect caused by #519

Ref #534

src/api/api.load.js
@@ -3,7 +3,7 @@
  * billboard.js project is licensed under the MIT license
  */
 import Chart from "../internals/Chart";
-import {extend, isString, isArray, isBoolean} from "../internals/util";
+import {extend, isString, isArray} from "../internals/util";
 
 extend(Chart.prototype, {
 	/**
@@ -80,7 +80,7 @@ extend(Chart.prototype, {
 		// unload if needed
 		if ("unload" in args && args.unload !== false) {
 			// TODO: do not unload if target will load (included in url/rows/columns)
-			$$.unload($$.mapToTargetIds(isBoolean(args.unload) && args.unload ? null : args.unload), () =>
+			$$.unload($$.mapToTargetIds(args.unload === true ? null : args.unload), () =>
 				$$.loadFromArgs(args)
 			);
 		} else {

src/axis/Axis.js
@@ -2,10 +2,15 @@
  * Copyright (c) 2017 NAVER Corp.
  * billboard.js project is licensed under the MIT license
  */
-import {select as d3Select} from "d3-selection";
-import {isFunction, isString, isValue, isEmpty, isNumber, isObjectType} from "../internals/util";
 import bbAxis from "./bb.axis";
 import CLASS from "../config/classes";
+import {capitalize, isFunction, isString, isValue, isEmpty, isNumber, isObjectType} from "../internals/util";
+
+const isHorizontal = ($$, forHorizontal) => {
+	const isRotated = $$.config.axis_rotated;
+
+	return forHorizontal ? isRotated : !isRotated;
+};
 
 export default class Axis {
 	constructor(owner) {
@@ -18,37 +23,34 @@ export default class Axis {
 		const isRotated = config.axis_rotated;
 		const main = $$.main;
 
-		$$.axes.x = main.append("g")
-			.attr("class", `${CLASS.axis} ${CLASS.axisX}`)
-			.attr("clip-path", $$.clipPathForXAxis)
-			.attr("transform", $$.getTranslate("x"))
-			.style("visibility", config.axis_x_show ? "visible" : "hidden");
+		["x", "y", "y2"].forEach(v => {
+			const axisStr = `axis${capitalize(v)}`;
+			const classAxis = `${CLASS.axis} ${CLASS[axisStr]}`;
+			const classLabel = CLASS[`${axisStr}Label`];
 
-		$$.axes.x.append("text")
-			.attr("class", CLASS.axisXLabel)
-			.attr("transform", isRotated ? "rotate(-90)" : "")
-			.style("text-anchor", this.textAnchorForXAxisLabel.bind(this));
+			$$.axes[v] = main.append("g")
+				.attr("class", classAxis)
+				.attr("clip-path", () => {
+					let res = null;
 
-		$$.axes.y = main.append("g")
-			.attr("class", `${CLASS.axis} ${CLASS.axisY}`)
-			.attr("clip-path", config.axis_y_inner ? "" : $$.clipPathForYAxis)
-			.attr("transform", $$.getTranslate("y"))
-			.style("visibility", config.axis_y_show ? "visible" : "hidden");
+					if (v === "x") {
+						res = $$.clipPathForXAxis;
+					} else if (v === "y" && config.axis_y_inner) {
+						res = $$.clipPathForYAxis;
+					}
 
-		$$.axes.y.append("text")
-			.attr("class", CLASS.axisYLabel)
-			.attr("transform", isRotated ? "" : "rotate(-90)")
-			.style("text-anchor", this.textAnchorForYAxisLabel.bind(this));
+					return res;
+				})
+				.attr("transform", $$.getTranslate(v))
+				.style("visibility", config[`axis_${v}_show`] ? "visible" : "hidden");
 
-		$$.axes.y2 = main.append("g")
-			.attr("class", `${CLASS.axis} ${CLASS.axisY2}`)
-			.attr("transform", $$.getTranslate("y2"))
-			.style("visibility", config.axis_y2_show ? "visible" : "hidden");
-
-		$$.axes.y2.append("text")
-			.attr("class", CLASS.axisY2Label)
-			.attr("transform", isRotated ? "" : "rotate(-90)")
-			.style("text-anchor", this.textAnchorForY2AxisLabel.bind(this));
+			$$.axes[v].append("text")
+				.attr("class", classLabel)
+				.attr("transform", ["rotate(-90)", null][
+					v === "x" ? +!isRotated : +isRotated
+				])
+				.style("text-anchor", this.textAnchorForXAxisLabel.bind(this));
+		});
 	}
 
 	getXAxis(axisName, scale, orient, tickFormat,
@@ -174,26 +176,28 @@ export default class Axis {
 			) : format;
 	}
 
-	getTickValues(tickValues, axis) {
+	getTickValues(type) {
+		const $$ = this.owner;
+		const tickValues = $$.config[`axis_${type}_tick_values`];
+		const axis = $$[`${type}Axis`];
+
 		return tickValues || (axis ? axis.tickValues() : undefined);
 	}
 
 	getXAxisTickValues() {
-		return this.getTickValues(this.owner.config.axis_x_tick_values, this.owner.xAxis);
+		return this.getTickValues("x");
 	}
 
 	getYAxisTickValues() {
-		return this.getTickValues(this.owner.config.axis_y_tick_values, this.owner.yAxis);
+		return this.getTickValues("y");
 	}
 
 	getY2AxisTickValues() {
-		return this.getTickValues(this.owner.config.axis_y2_tick_values, this.owner.y2Axis);
+		return this.getTickValues("y2");
 	}
 
 	getLabelOptionByAxisId(axisId) {
-		const $$ = this.owner;
-
-		return $$.config[`axis_${axisId}_label`];
+		return this.owner.config[`axis_${axisId}_label`];
 	}
 
 	getLabelText(axisId) {
@@ -217,32 +221,35 @@ export default class Axis {
 	}
 
 	getLabelPosition(axisId, defaultPosition) {
+		const isRotated = this.owner.config.axis_rotated;
 		const option = this.getLabelOptionByAxisId(axisId);
 		const position = (isObjectType(option) && option.position) ?
-			option.position : defaultPosition;
+			option.position : defaultPosition[+!isRotated];
+
+		const has = v => !!~position.indexOf(v);
 
 		return {
-			isInner: !!~position.indexOf("inner"),
-			isOuter: !!~position.indexOf("outer"),
-			isLeft: !!~position.indexOf("left"),
-			isCenter: !!~position.indexOf("center"),
-			isRight: !!~position.indexOf("right"),
-			isTop: !!~position.indexOf("top"),
-			isMiddle: !!~position.indexOf("middle"),
-			isBottom: !!~position.indexOf("bottom"),
+			isInner: has("inner"),
+			isOuter: has("outer"),
+			isLeft: has("left"),
+			isCenter: has("center"),
+			isRight: has("right"),
+			isTop: has("top"),
+			isMiddle: has("middle"),
+			isBottom: has("bottom")
 		};
 	}
 
 	getXAxisLabelPosition() {
-		return this.getLabelPosition("x", this.owner.config.axis_rotated ? "inner-top" : "inner-right");
+		return this.getLabelPosition("x", ["inner-top", "inner-right"]);
 	}
 
 	getYAxisLabelPosition() {
-		return this.getLabelPosition("y", this.owner.config.axis_rotated ? "inner-right" : "inner-top");
+		return this.getLabelPosition("y", ["inner-right", "inner-top"]);
 	}
 
 	getY2AxisLabelPosition() {
-		return this.getLabelPosition("y2", this.owner.config.axis_rotated ? "inner-right" : "inner-top");
+		return this.getLabelPosition("y2", ["inner-right", "inner-top"]);
 	}
 
 	getLabelPositionById(id) {
@@ -261,11 +268,11 @@ export default class Axis {
 		return this.getLabelText("y2");
 	}
 
-	xForAxisLabel(forHorizontal, position) {
+	xForAxisLabel(position, forHorizontal = true) {
 		const $$ = this.owner;
 		let x = position.isMiddle ? -$$.height / 2 : 0;
 
-		if (forHorizontal) {
+		if (isHorizontal($$, forHorizontal)) {
 			x = position.isLeft ? 0 : (
 				position.isCenter ? $$.width / 2 : $$.width
 			);
@@ -276,10 +283,11 @@ export default class Axis {
 		return x;
 	}
 
-	dxForAxisLabel(forHorizontal, position) {
+	dxForAxisLabel(position, forHorizontal = true) {
+		const $$ = this.owner;
 		let dx = position.isBottom ? "0.5em" : "0";
 
-		if (forHorizontal) {
+		if (isHorizontal($$, forHorizontal)) {
 			dx = position.isLeft ? "0.5em" : (
 				position.isRight ? "-0.5em" : "0"
 			);
@@ -290,10 +298,11 @@ export default class Axis {
 		return dx;
 	}
 
-	textAnchorForAxisLabel(forHorizontal, position) {
+	textAnchorForAxisLabel(position, forHorizontal = true) {
+		const $$ = this.owner;
 		let anchor = position.isMiddle ? "middle" : "end";
 
-		if (forHorizontal) {
+		if (isHorizontal($$, forHorizontal)) {
 			anchor = position.isLeft ? "start" : (
 				position.isCenter ? "middle" : "end"
 			);
@@ -305,27 +314,27 @@ export default class Axis {
 	}
 
 	xForXAxisLabel() {
-		return this.xForAxisLabel(!this.owner.config.axis_rotated, this.getXAxisLabelPosition());
+		return this.xForAxisLabel(this.getXAxisLabelPosition(), false);
 	}
 
 	xForYAxisLabel() {
-		return this.xForAxisLabel(this.owner.config.axis_rotated, this.getYAxisLabelPosition());
+		return this.xForAxisLabel(this.getYAxisLabelPosition());
 	}
 
 	xForY2AxisLabel() {
-		return this.xForAxisLabel(this.owner.config.axis_rotated, this.getY2AxisLabelPosition());
+		return this.xForAxisLabel(this.getY2AxisLabelPosition());
 	}
 
 	dxForXAxisLabel() {
-		return this.dxForAxisLabel(!this.owner.config.axis_rotated, this.getXAxisLabelPosition());
+		return this.dxForAxisLabel(this.getXAxisLabelPosition(), false);
 	}
 
 	dxForYAxisLabel() {
-		return this.dxForAxisLabel(this.owner.config.axis_rotated, this.getYAxisLabelPosition());
+		return this.dxForAxisLabel(this.getYAxisLabelPosition());
 	}
 
 	dxForY2AxisLabel() {
-		return this.dxForAxisLabel(this.owner.config.axis_rotated, this.getY2AxisLabelPosition());
+		return this.dxForAxisLabel(this.getY2AxisLabelPosition());
 	}
 
 	dyForXAxisLabel() {
@@ -368,94 +377,62 @@ export default class Axis {
 	}
 
 	textAnchorForXAxisLabel() {
-		const isRotated = this.owner.config.axis_rotated;
-
-		return this.textAnchorForAxisLabel(!isRotated, this.getXAxisLabelPosition());
+		return this.textAnchorForAxisLabel(this.getXAxisLabelPosition(), false);
 	}
 
 	textAnchorForYAxisLabel() {
-		const isRotated = this.owner.config.axis_rotated;
-
-		return this.textAnchorForAxisLabel(isRotated, this.getYAxisLabelPosition());
+		return this.textAnchorForAxisLabel(this.getYAxisLabelPosition());
 	}
 
 	textAnchorForY2AxisLabel() {
-		const isRotated = this.owner.config.axis_rotated;
-
-		return this.textAnchorForAxisLabel(isRotated, this.getY2AxisLabelPosition());
+		return this.textAnchorForAxisLabel(this.getY2AxisLabelPosition());
 	}
 
 	getMaxTickWidth(id, withoutRecompute) {
 		const $$ = this.owner;
 		const config = $$.config;
+		const currentTickMax = $$.currentMaxTickWidths;
 		let maxWidth = 0;
-		let dummy;
-		let svg;
 
-		if (withoutRecompute && $$.currentMaxTickWidths[id]) {
-			return $$.currentMaxTickWidths[id];
+		if (withoutRecompute && currentTickMax[id]) {
+			return currentTickMax[id];
 		}
 
 		if ($$.svg) {
+			const isYAxis = /^y2?$/.test(id);
 			const targetsToShow = $$.filterTargetsToShow($$.data.targets);
-			let scale;
-			let axis;
+			const getFrom = isYAxis ? "getY" : "getX";
 
-			if (/^y2?$/.test(id)) {
-				scale = $$[id].copy().domain($$.getYDomain(targetsToShow, id));
-				axis = this.getYAxis(
-					id,
-					scale,
-					$$[`${id}Orient`],
-					config[`axis_${id}_tick_format`],
-					$$[`${id}AxisTickValues`],
-					false,
-					true,
-					true
-				);
-			} else {
-				scale = $$.x.copy().domain($$.getXDomain(targetsToShow));
-				axis = this.getXAxis(
-					"x",
-					scale,
-					$$.xOrient,
-					$$.xAxisTickFormat,
-					$$.xAxisTickValues,
-					false,
-					true,
-					true
-				);
+			const scale = $$[id].copy().domain($$[`${getFrom}Domain`](targetsToShow, id));
+			const axis = this[`${getFrom}Axis`](
+				id,
+				scale,
+				$$[`${id}Orient`],
+				isYAxis ? config[`axis_${id}_tick_format`] : $$.xAxisTickFormat,
+				null,
+				false,
+				true,
+				true
+			);
 
-				this.updateXAxisTickValues(targetsToShow, axis);
-			}
+			!isYAxis && this.updateXAxisTickValues(targetsToShow, axis);
 
-			dummy = d3Select("body")
-				.append("div")
-				.classed("bb", true);
+			const dummy = $$.selectChart.append("svg")
+				.style("visibility", "hidden");
 
-			svg = dummy.append("svg")
-				.style("visibility", "hidden")
-				.style("position", "fixed")
-				.style("top", "0px")
-				.style("left", "0px");
-
-			svg.append("g").call(axis)
+			dummy.call(axis).selectAll("text")
 				.each(function() {
-					d3Select(this).selectAll("text")
-						.each(function() {
-							const boxWidth = this.getBoundingClientRect().width;
-
-							maxWidth < boxWidth && (maxWidth = boxWidth);
-						});
-
-					dummy.remove();
+					maxWidth = Math.max(maxWidth, this.getBoundingClientRect().width);
 				});
+
+			dummy.remove();
 		}
 
-		$$.currentMaxTickWidths[id] = maxWidth <= 0 ?
-			$$.currentMaxTickWidths[id] : maxWidth;
+		if (maxWidth > 0) {
+			currentTickMax[id] = maxWidth;
+		}
 
-		return $$.currentMaxTickWidths[id];
+		return currentTickMax[id];
 	}
 
 	updateLabels(withTransition) {
@@ -466,14 +443,15 @@ export default class Axis {
 			Y2: $$.main.select(`.${CLASS.axisY2} .${CLASS.axisY2Label}`)
 		};
 
-		Object.keys(labels).forEach(axis => {
-			const node = labels[axis];
+		Object.keys(labels).forEach(v => {
+			const node = labels[v];
+			const axisLabel = `${v}AxisLabel`;
 
 			(withTransition ? node.transition() : node)
-				.attr("x", this[`xFor${axis}AxisLabel`].bind(this))
-				.attr("dx", this[`dxFor${axis}AxisLabel`].bind(this))
-				.attr("dy", this[`dyFor${axis}AxisLabel`].bind(this))
-				.text(this[`textFor${axis}AxisLabel`].bind(this));
+				.attr("x", this[`xFor${axisLabel}`].bind(this))
+				.attr("dx", this[`dxFor${axisLabel}`].bind(this))
+				.attr("dy", this[`dyFor${axisLabel}`].bind(this))
+				.text(this[`textFor${axisLabel}`].bind(this));
 		});
 	}
 
@@ -545,22 +523,19 @@ export default class Axis {
 		const $$ = this.owner;
 		const axes = $$.axes;
 
-		return {
-			axisX: duration ? axes.x.transition().duration(duration) : axes.x,
-			axisY: duration ? axes.y.transition().duration(duration) : axes.y,
-			axisY2: duration ? axes.y2.transition().duration(duration) : axes.y2,
-			axisSubX: duration ? axes.subx.transition().duration(duration) : axes.subx,
-		};
+		const [axisX, axisY, axisY2, axisSubX] = ["x", "y", "y2", "subx"]
+			.map(v => (duration ? axes[v].transition().duration(duration) : axes[v]));
+
+		return {axisX, axisY, axisY2, axisSubX};
 	}
 
 	redraw(transitions, isHidden) {
 		const $$ = this.owner;
 		const opacity = isHidden ? "0" : "1";
 
-		$$.axes.x.style("opacity", opacity);
-		$$.axes.y.style("opacity", opacity);
-		$$.axes.y2.style("opacity", opacity);
-		$$.axes.subx.style("opacity", opacity);
+		["x", "y", "y2", "subx"].forEach(v => {
+			$$.axes[v].style("opacity", opacity);
+		});
 
 		transitions.axisX.call($$.xAxis);
 		transitions.axisY.call($$.yAxis);

src/data/data.convert.js
@@ -287,10 +287,10 @@ extend(ChartInternal.prototype, {
 			}
 
 			// indexing each value
-			let i = 0;
+			t.values.forEach((v, i) => {
+				const index = $$.data.targets ? $$.getIndexByX(v.x) : null;
 
-			t.values.forEach(v => {
-				v.index = $$.data.targets ? $$.getIndexByX(v.x) : i++;
+				v.index = index === null ? i : index;
 			});
 
 			// this needs to be sorted because its index and value.index is identical

src/data/data.js
@@ -130,7 +130,7 @@ extend(ChartInternal.prototype, {
 	},
 
 	getValueOnIndex(values, index) {
-		const valueOnIndex = values.filter(v => v.index === index);
+		const valueOnIndex = values.filter(v => (v.index === null ? v.x : v.index) === index);
 
 		return valueOnIndex.length ? valueOnIndex[0] : null;
 	},

src/interactions/interaction.js
@@ -216,41 +216,48 @@ extend(ChartInternal.prototype, {
 				// update index for x that is used by prevX and nextX
 				$$.updateXs();
 
+				const getPrevNextX = d => {
+					const index = d.index;
+
+					return {
+						prev: $$.getPrevX(index),
+						next: $$.getNextX(index)
+					};
+				};
+
 				rectW = d => {
-					let prevX = $$.getPrevX(d.index);
-					let nextX = $$.getNextX(d.index);
+					const x = getPrevNextX(d);
 
 					// if there this is a single data point make the eventRect full width (or height)
-					if (prevX === null && nextX === null) {
+					if (x.prev === null && x.next === null) {
 						return isRotated ? $$.height : $$.width;
 					}
 
-					if (prevX === null) {
-						prevX = xScale.domain()[0];
+					if (x.prev === null) {
+						x.prev = xScale.domain()[0];
 					}
 
-					if (nextX === null) {
-						nextX = xScale.domain()[1];
+					if (x.next === null) {
+						x.next = xScale.domain()[1];
 					}
 
-					return Math.max(0, (xScale(nextX) - xScale(prevX)) / 2);
+					return Math.max(0, (xScale(x.next) - xScale(x.prev)) / 2);
 				};
 
 				rectX = d => {
-					const nextX = $$.getNextX(d.index);
+					const x = getPrevNextX(d);
 					const thisX = $$.data.xs[d.id][d.index];
-					let prevX = $$.getPrevX(d.index);
 
 					// if there this is a single data point position the eventRect at 0
-					if (prevX === null && nextX === null) {
+					if (x.prev === null && x.next === null) {
 						return 0;
 					}
 
-					if (prevX === null) {
-						prevX = xScale.domain()[0];
+					if (x.prev === null) {
+						x.prev = xScale.domain()[0];
 					}
 
-					return (xScale(thisX) + xScale(prevX)) / 2;
+					return (xScale(thisX) + xScale(x.prev)) / 2;
 				};
 			}
 

spec/api/api.load-spec.js
@@ -223,4 +223,59 @@ describe("API load", function() {
 			});
 		});
 	});
+
+	describe("y Axis Label", () => {
+		before(() => {
+			args = {
+				data: {
+					columns: [
+						['data1', 30, 200, 100, 400, 150],
+						['data2', 45, 423, 356, 478, 166]
+					],
+					axes: {
+						data1: "y",
+						data2: "y2"
+					}
+				},
+				axis: {
+					y: {
+						label: {
+							text: "Y Label",
+								position: "outer-middle"
+						},
+						tick: {
+							count: 5,
+							format: d3.format("$,")
+						}
+					},
+					y2:{
+						show:true,
+							label: {
+							text: "Y2 Label",
+							position: "outer-middle"
+						}
+					}
+				}
+			}
+		});
+
+		it("should be updated the axis label position ", done => {
+			const axisLabel = chart.$.main.select(`.${CLASS.axisYLabel}`);
+			const dy = +axisLabel.attr("dy");
+
+			chart.load({
+				columns: [
+					["data5", 2300000, 1900000, 3000000, 5000000, 3000000]
+				],
+				unload: ["data1"],
+				done: () => {
+					setTimeout(() => {
+						expect(+axisLabel.attr("dy")).to.be.below(dy);
+						done();
+					}, 500);
+				}
+			});
+
+		})
+	});
 });