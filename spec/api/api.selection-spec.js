eb3aeb6dc3f843045a8584c65d1626cdf2034a00
Jae Sung Park <alberto.park@gmail.com>
2018-11-01 18:50:06 +0900

    test(api): Add test code for bar type using .select()
    Test cases for bar(path) elements selection
    
    Ref #617

--- spec/api/api.selection-spec.js
+++ spec/api/api.selection-spec.js
@@ -7,7 +7,9 @@ import util from "../assets/util";
 import CLASS from "../../src/config/classes";
 
 describe("API select", () => {
-	const chart = util.generate({
+	let chart;
+	let main;
+	let args = {
 		data: {
 			columns: [
 				["data1", 30, 200, 100, 400, 150],
@@ -17,60 +19,130 @@ describe("API select", () => {
 				enabled: true
 			}
 		}
+	};
+
+	describe("selection for circle data points", () => {
+		before(() => {
+			chart = util.generate(args);
+			main = chart.$.main;
+		});
+
+		it("should select all data points", () => {
+			chart.select();
+
+			const selected = main.selectAll(`.${CLASS.selectedCircle}`);
+			const dataLen = chart.data.values("data1").length + chart.data.values("data2").length;
+
+			expect(selected.size()).to.be.equal(dataLen);
+		});
+
+		it("should unselect indice '1' data point", done => {
+			const indice = 1;
+
+			chart.unselect(["data1", "data2"], [indice]);
+
+			setTimeout(() => {
+				const unselected = main.selectAll(`.${CLASS.selectedCircle}`)
+					.filter(`.${CLASS.selectedCircle}-${indice}`);
+
+				expect(unselected.empty()).to.be.ok;
+
+				done();
+			}, 500);
+		});
+
+		it("should unselect all data points", done => {
+			chart.unselect();
+
+			setTimeout(() => {
+				const unselected = main.selectAll(`.${CLASS.selectedCircle}`);
+
+				expect(unselected.empty()).to.be.ok;
+
+				done();
+			}, 500);
+		});
+
+		it("should select some portion of data points", done => {
+			const indice = [1, 3];
+
+			chart.select("data1", indice, true);
+
+			const selected = chart.selected();
+
+			setTimeout(() => {
+				main.selectAll(`.${CLASS.selectedCircles}-data1 circle`).each((v, i) => {
+					expect(v).to.be.equal(selected[i]);
+					expect(v.index).to.be.equal(indice[i]);
+				});
+
+				done();
+			}, 500);
+		});
 	});
-	const main = chart.internal.main;
 
-	it("should select all data points", () => {
-		chart.select();
+	describe("selection for bar", () => {
+		before(() => {
+			args.data.type = "bar";
+			chart = util.generate(args);
+			main = chart.$.main;
+		});
 
-		const selected = main.selectAll(`.${CLASS.selectedCircle}`);
-		const dataLen = chart.data.values("data1").length + chart.data.values("data2").length;
+		it("should select all data points", () => {
+			chart.select();
 
-		expect(selected.size()).to.be.equal(dataLen);
-	});
+			const selected = main.selectAll(`.${CLASS.SELECTED}`);
+			const dataLen = chart.data.values("data1").length + chart.data.values("data2").length;
 
-	it("should unselect indice '1' data point", done => {
-		const indice = 1;
+			expect(selected.size()).to.be.equal(dataLen);
+		});
 
-		chart.unselect(["data1", "data2"], [indice]);
+		it("should unselect indice '1' data point", done => {
+			const indice = 1;
 
-		setTimeout(() => {
-			const unselected = main.selectAll(`.${CLASS.selectedCircle}`)
-				.filter(`.${CLASS.selectedCircle}-${indice}`);
+			chart.unselect(["data1", "data2"], [indice]);
 
-			expect(unselected.empty()).to.be.ok;
+			setTimeout(() => {
+				const unselected = main.selectAll(`.${CLASS.SELECTED}`)
+					.filter(`.${CLASS.bar}-${indice}`);
 
-			done();
-		}, 500);
-	});
+				expect(unselected.empty()).to.be.ok;
 
-	it("should unselect all data points", done => {
-		chart.unselect();
+				done();
+			}, 500);
+		});
 
-		setTimeout(() => {
-			const unselected = main.selectAll(`.${CLASS.selectedCircle}`);
+		it("should unselect all data points", done => {
+			chart.unselect();
 
-			expect(unselected.empty()).to.be.ok;
+			setTimeout(() => {
+				const unselected = main.selectAll(`.${CLASS.SELECTED}`);
 
-			done();
-		}, 500);
-	});
+				expect(unselected.empty()).to.be.ok;
 
-	it("should select some portion of data points", done => {
-		const indice = [1, 3];
+				done();
+			}, 500);
+		});
 
-		chart.select();
-		chart.select("data1", indice, true);
+		it("should select some portion of data points", done => {
+			const indice = [1, 3];
+			const color = chart.color("data1");
 
-		const selected = chart.selected();
+			chart.select("data1", indice, true);
 
-		setTimeout(() => {
-			main.selectAll(`.${CLASS.selectedCircles}-data1 circle`).each((v, i) => {
-				expect(v).to.be.equal(selected[i]);
-				expect(v.index).to.be.equal(indice[i]);
-			});
+			const selected = chart.selected();
 
-			done();
-		}, 500);
+			setTimeout(() => {
+				main.selectAll(`.${CLASS.shapes}-data1 path.${CLASS.SELECTED}`).each(function(v, i) {
+					expect(v).to.be.equal(selected[i]);
+					expect(v.index).to.be.equal(indice[i]);
+
+					// check for the selected color
+					expect(this.style.fill).to.be.equal(d3.rgb(color).brighter(0.75).toString());
+				});
+
+				done();
+			}, 500);
+		});
 	});
 });
