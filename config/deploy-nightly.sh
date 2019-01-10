38665e4c55f9b69e1e028021f6de6a84bacfd3fc
Travis CI <travis@travis-ci.org>
2019-01-10 13:50:41 +0900

    skip: setup for nightly build

---
+++ config/deploy-nightly.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Credit: https://www.vinaygopinath.me/blog/tech/commit-to-master-branch-on-github-using-travis-ci/
+
+# set nightly version
+VERSION="$(node -pe "require('./package.json').version")-`date '+%Y%m%d%H%M%S'`"
+VERSION="${VERSION/snapshot/nightly}"
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+build_nightly() {
+  ./node_modules/.bin/cross-env NIGHTLY=$VERSION npm run build:production
+  ./node_modules/.bin/cross-env NIGHTLY=$VERSION npm run build:packaged
+  ./node_modules/.bin/cross-env NIGHTLY=$VERSION npm run build:theme
+}
+
+build_and_commit() {
+  git checkout nightly
+  git fetch origin
+  git merge origin/master --no-verify
+
+  build_nightly
+  git commit -a -m "skip: $VERSION build [skip ci]"
+}
+
+push() {
+  # Remove existing "origin"
+  git remote rm origin
+
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://netil:${GH_TOKEN}@github.com/naver/billboard.js.git > /dev/null 2>&1
+  git push origin nightly
+}
+
+setup_git
+build_and_commit
+push
