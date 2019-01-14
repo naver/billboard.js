6127a3d591456ad72d37fc8aac2c31ffa451ebf6
Travis CI <travis@travis-ci.org>
2019-01-14 02:23:45 +0000

    skip: 1.7.1-nightly-20190114022259 build [skip ci]

--- dist/billboard.pkgd.js
+++ dist/billboard.pkgd.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * http://naver.github.io/billboard.js/
  * 
- * @version 1.7.1-nightly-20190114021653
+ * @version 1.7.1-nightly-20190114022259
  * 
  * All-in-one packaged file for ease use of 'billboard.js' with below dependency.
  * - d3 ^5.7.0
@@ -23602,7 +23602,7 @@ util_extend(Chart_Chart.prototype, {
 
 /**
  * @namespace bb
- * @version 1.7.1-nightly-20190114021653
+ * @version 1.7.1-nightly-20190114022259
  */
 
 var bb = {
@@ -23613,7 +23613,7 @@ var bb = {
    *    bb.version;  // "1.0.0"
    * @memberof bb
    */
-  version: "1.7.1-nightly-20190114021653",
+  version: "1.7.1-nightly-20190114022259",
 
   /**
    * Generate chart
