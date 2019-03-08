905fe88b405a9c0f846f07ffb4e7706e07c2b16d
Jae Sung Park <alberto.park@gmail.com>
2019-03-08 16:48:20 +0900

    docs(demo): Add rotated axis grouped bar demo [skip ci]

--- demo/demo.js
+++ demo/demo.js
@@ -934,6 +934,57 @@ var demos = {
 				}
 			}
 		},
+		RotatedAxisGroupedBar: {
+			options: {
+				data: {
+					columns: [
+						["Male", -83, -143, -100, -120, -150, -85],
+						["Female", 130, 100, 140, 175, 150, 50]
+					],
+					type: "bar",
+					groups: [["Male", "Female"]],
+					labels: {
+						format: function(v, id) {
+							return Math.abs(v);
+						}
+					}
+				},
+				axis: {
+					rotated: true,
+					x: {
+						show: false
+					},
+					y: {
+						tick: {
+							format: function(v) {
+								return Math.abs(v);
+							}
+						}
+					}
+				},
+				grid: {
+					y: {
+						show: true,
+						lines: [
+							{
+								value: 0,
+								class: "base-line"
+							},
+						]
+					}
+				},
+				tooltip: {
+					format: {
+						value: function(v) {
+							return Math.abs(v);
+						}
+					}
+				}
+			},
+			style: [
+				"#RotatedAxisGroupedBar .base-line line { stroke-width: 3px; stroke: #000; }"
+			]
+		},
 		RotateXAxisTickText: {
 			options: {
 				data: {
