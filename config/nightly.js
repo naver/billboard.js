f83e77b4bd9cc2b7818c05873cc3340cab3796fe
Jae Sung Park <alberto.park@gmail.com>
2019-04-30 10:44:37 +0900

    skip: update nightly build script

--- config/nightly.js
+++ config/nightly.js
@@ -22,6 +22,7 @@ function getDatetime() {
 
 // is deploy build? (The deploy is intended to be ran from Travis CI)
 const deploy = process.env.DEPLOY_NIGHTLY;
+// const token = process.env.GH_TOKEN;
 
 // set version for nightly
 const version = pkg.version.replace(/snapshot/, `nightly-${getDatetime()}`);
@@ -29,17 +30,24 @@ let cmd = `cross-env NIGHTLY=${version} npm run build:`;
 
 // build command
 const build = {
-	merge_master: deploy ? "git fetch origin && git merge origin/master --no-verify" : "",
 	production: `${cmd}production`,
 	packaged: `${cmd}packaged`,
 	theme: `${cmd}theme`,
-	push: deploy ? `git commit -a -m "skip: ${version} build" && git push origin nightly` : ""
+	plugin: `${cmd}plugin`
 };
+
+if (deploy) {
+	build.push = `git commit -a -m "skip: ${version} build [skip ci]"`;
+}
 
 cmd = Object.values(build);
 
 console.log(`***** Starting build v${version} *****\r\n> ${cmd.join("\r\n> ")}`);
 
-exec(cmd.join(" && "), () => {
-	console.log(`***** Finished successfully! *****`);
+exec(cmd.join(" && "), (error, stdout, stderr) => {
+	if (error || stderr) {
+		console.error(error, stderr);
+	} else {
+		console.log(`***** ${stdout} : Finished successfully! *****`);
+	}
 });
