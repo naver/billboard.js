naver/billboard.js

commit c1af5dfaa20b6ff2775ebb2fc9537bc2160dd53c

feat(tooltip): Intent to ship tooltip.contents.template
- Implementation of tooltip.contents.template
- Update legend template processing with util's .tplProcess()
- Update dependencies to the latest

Fix #813

src/internals/tooltip.js
@@ -2,10 +2,13 @@
  * Copyright (c) 2017 ~ present NAVER Corp.
  * billboard.js project is licensed under the MIT license
  */
-import {mouse as d3Mouse} from "d3-selection";
+import {
+	select as d3Select,
+	mouse as d3Mouse
+} from "d3-selection";
 import ChartInternal from "./ChartInternal";
 import CLASS from "../config/classes";
-import {extend, isFunction, isString, isValue, callFn, sanitise} from "./util";
+import {extend, isFunction, isString, isValue, callFn, sanitise, tplProcess} from "./util";
 
 extend(ChartInternal.prototype, {
 	/**
@@ -15,14 +18,19 @@ extend(ChartInternal.prototype, {
 	initTooltip() {
 		const $$ = this;
 		const config = $$.config;
+		const bindto = config.tooltip_contents.bindto;
 
-		$$.tooltip = $$.selectChart
-			.style("position", "relative")
-			.append("div")
-			.attr("class", CLASS.tooltipContainer)
-			.style("position", "absolute")
-			.style("pointer-events", "none")
-			.style("display", "none");
+		$$.tooltip = d3Select(bindto);
+
+		if ($$.tooltip.empty()) {
+			$$.tooltip = $$.selectChart
+				.style("position", "relative")
+				.append("div")
+				.attr("class", CLASS.tooltipContainer)
+				.style("position", "absolute")
+				.style("pointer-events", "none")
+				.style("display", "none");
+		}
 
 		// Show tooltip if needed
 		if (config.tooltip_init_show) {
@@ -42,14 +50,33 @@ extend(ChartInternal.prototype, {
 				config.tooltip_init_x = i;
 			}
 
-			$$.tooltip.html(config.tooltip_contents.call($$,
+			$$.tooltip.html($$.getTooltipHTML(
 				$$.data.targets.map(d => $$.addName(d.values[config.tooltip_init_x])),
-				$$.axis.getXAxisTickFormat(), $$.getYFormat($$.hasArcType(null, ["radar"])), $$.color));
+				$$.axis.getXAxisTickFormat(),
+				$$.getYFormat($$.hasArcType(null, ["radar"])),
+				$$.color
+			));
 
-			$$.tooltip.style("top", config.tooltip_init_position.top)
-				.style("left", config.tooltip_init_position.left)
-				.style("display", "block");
+			if (!bindto) {
+				$$.tooltip.style("top", config.tooltip_init_position.top)
+					.style("left", config.tooltip_init_position.left)
+					.style("display", "block");
+			}
 		}
+	},
+
+	/**
+	 * Get the tooltip HTML string
+	 * @param  {...any} args
+	 * @private
+	 * @return {String} Formatted HTML string
+	 */
+	getTooltipHTML(...args) {
+		const $$ = this;
+		const config = $$.config;
+
+		return isFunction(config.tooltip_contents) ?
+			config.tooltip_contents.call($$, ...args) : $$.getTooltipContent(...args);
 	},
 
 	/**
@@ -70,6 +97,7 @@ extend(ChartInternal.prototype, {
 		const order = config.tooltip_order;
 		const getRowValue = row => $$.getBaseValue(row);
 		const getBgColor = $$.levelColor ? row => $$.levelColor(row.value) : row => color(row.id);
+		const tplStr = config.tooltip_contents.template;
 
 		if (order === null && config.data_groups.length) {
 			// for stacked data, order should aligned with the visually displayed data
@@ -101,22 +129,26 @@ extend(ChartInternal.prototype, {
 			d.sort(order);
 		}
 
+		const tpl = $$.getTooltipContentTemplate(tplStr);
 		let text;
 		let row;
 		let param;
 		let value;
 
-		for (let i = 0, len = d.length; i < len; i++) {
-			if (!((row = d[i]) && (getRowValue(row) || getRowValue(row) === 0))) {
+		for (let i = 0; (row = d[i]); i++) {
+			if (!(getRowValue(row) || getRowValue(row) === 0)) {
 				continue;
 			}
 
-			if (!text) {
+			if (i === 0) {
 				const title = sanitise(titleFormat ? titleFormat(row.x) : row.x);
 
-				text = `<table class="${CLASS.tooltip}">${
-					isValue(title) ? `<tr><th colspan="2">${title}</th></tr>` : ""
-				}`;
+				text = tplProcess(tpl[0], {
+					CLASS_TOOLTIP: CLASS.tooltip,
+					TITLE: isValue(title) ? (
+						tplStr ? title : `<tr><th colspan="2">${title}</th></tr>`
+					) : ""
+				});
 			}
 
 			param = [row.ratio, row.id, row.index, d];
@@ -137,19 +169,38 @@ extend(ChartInternal.prototype, {
 				}
 
 				const name = sanitise(nameFormat(row.name, ...param));
-				const bgcolor = getBgColor(row);
+				const color = getBgColor(row);
 
-				text += `<tr class="${CLASS.tooltipName}${$$.getTargetSelectorSuffix(row.id)}"><td class="name">`;
-
-				text += $$.patterns ?
-					`<svg><rect style="fill:${bgcolor}" width="10" height="10"></rect></svg>` :
-					`<span style="background-color:${bgcolor}"></span>`;
-
-				text += `${name}</td><td class="value">${value}</td></tr>`;
+				text += tplProcess(tpl[1], {
+					CLASS_TOOLTIP_NAME: CLASS.tooltipName + $$.getTargetSelectorSuffix(row.id),
+					COLOR: tplStr ? color : (
+						$$.patterns ? `<svg><rect style="fill:${color}" width="10" height="10"></rect></svg>` :
+							`<span style="background-color:${color}"></span>`),
+					"NAME": name,
+					VALUE: value
+				});
 			}
 		}
 
 		return `${text}</table>`;
+	},
+
+	/**
+	 * Get the content template string
+	 * @param {String} tplStr
+	 * @return {String} Template string
+	 * @private
+	 */
+	getTooltipContentTemplate(tplStr) {
+		return (tplStr || `<table class="{=CLASS_TOOLTIP}"><tbody>
+				{=TITLE}
+				{{<tr class="{=CLASS_TOOLTIP_NAME}">
+					<td class="name">{=COLOR}{=NAME}</td>
+					<td class="value">{=VALUE}</td>
+				</tr>}}
+			</tbody></table>`)
+			.replace(/(\r?\n|\t)/g, "")
+			.split(/{{(.*)}}/);
 	},
 
 	/**
@@ -219,6 +270,7 @@ extend(ChartInternal.prototype, {
 	showTooltip(selectedData, element) {
 		const $$ = this;
 		const config = $$.config;
+		const bindto = config.tooltip_contents.bindto;
 		const forArc = $$.hasArcType(null, ["radar"]);
 		const dataToShow = selectedData.filter(d => d && isValue($$.getBaseValue(d)));
 		const positionFunction = config.tooltip_position || $$.tooltipPosition;
@@ -234,19 +286,18 @@ extend(ChartInternal.prototype, {
 
 		if (!datum || datum.current !== dataStr) {
 			const index = selectedData.concat().sort()[0].index;
-			const html = config.tooltip_contents.call(
-				$$,
-				selectedData,
-				$$.axis.getXAxisTickFormat(),
-				$$.getYFormat(forArc),
-				$$.color
-			);
 
 			callFn(config.tooltip_onshow, $$);
 
 			// set tooltip content
-			$$.tooltip.html(html)
-				.style("display", "block")
+			$$.tooltip
+				.html($$.getTooltipHTML(
+					selectedData,
+					$$.axis.getXAxisTickFormat(),
+					$$.getYFormat(forArc),
+					$$.color
+				))
+				.style("display", config.tooltip_doNotHide === false ? "block" : null)
 				.datum({
 					index,
 					current: dataStr,
@@ -258,13 +309,15 @@ extend(ChartInternal.prototype, {
 			$$._handleLinkedCharts(true, index);
 		}
 
-		// Get tooltip dimensions
-		const position = positionFunction.call(this, dataToShow, width, height, element);
+		if (!bindto) {
+			// Get tooltip dimensions
+			const position = positionFunction.call(this, dataToShow, width, height, element);
 
-		// Set tooltip position
-		$$.tooltip
-			.style("top", `${position.top}px`)
-			.style("left", `${position.left}px`);
+			// Set tooltip position
+			$$.tooltip
+				.style("top", `${position.top}px`)
+				.style("left", `${position.left}px`);
+		}
 	},
 
 	/**

spec/internals/tooltip-spec.js
@@ -711,12 +711,18 @@ describe("TOOLTIP", function() {
 
 	describe("tooltip display", () => {
 		before(() => {
+			sandbox("tooltip-wrapper").innerHTML = "<div id='tooltip'></div>";
+
 			args = {
 				data: {
 					columns: [
 						["data1", 30, 200, 100],
 						["data2", 130, 100, 140]
-					]
+					],
+					colors: {
+						data1: "#00c73c",
+						data2: "#fa7171"
+					}
 				},
 				tooltip: {
 					doNotHide: true
@@ -733,6 +739,25 @@ describe("TOOLTIP", function() {
 			// when is called .hide(), it should be hide
 			chart.tooltip.hide();
 			expect(chart.$.tooltip.style("display")).to.be.equal("none");
+		});
+
+		it("set options tooltip.contents", () => {
+			args.tooltip.contents = {
+				bindto: "#tooltip",
+				template: `<ul><li>Index<br>{=TITLE}</li>
+					{{<li class={=CLASS_TOOLTIP_NAME}>
+					<span>{=VALUE}</span><br>
+					<span style=color:{=COLOR}>{=NAME}</span></li>}}</ul>`
+			};
+		});
+
+		it("check for tooltip contents template", () => {
+			const html = `<ul><li>Index<br>2</li><li class="bb-tooltip-name-data1"><span>100</span><br><span style="color:#00c73c">data1</span></li><li class="bb-tooltip-name-data2"><span>140</span><br><span style="color:#fa7171">data2</span></li></ul>`;
+
+			util.hoverChart(chart, "mousemove", {clientX: 185, clientY: 107});
+			util.hoverChart(chart, "mouseout", {clientX: -100, clientY: -100});
+
+			expect(d3.select("#tooltip").html()).to.be.equal(html);
 		});
 	});
 });

demo/chart.js
@@ -157,13 +157,19 @@ var billboardDemo = {
 		return false;
 	},
 
+	getLowerFirstCase(str) {
+		return str.charAt(0).toLowerCase() + str.slice(1);
+	},
+
 	_addChartInstance: function(type, key, index, code) {
+		key = this.getLowerFirstCase(key);
+
 		if (index) {
 			key += "_"+ index;
 		}
 
 		var $el = document.getElementById(key);
-		var legend;
+		var template;
 
 		if (!$el) {
 			$el = document.createElement("div");
@@ -175,13 +181,14 @@ var billboardDemo = {
 
 			this.$chartArea.appendChild($el);
 
-			if (key.indexOf("LegendTemplate") > -1) {
-				legend = document.createElement("div");
-				legend.id = "legend";
-				legend.style.textAlign = "center";
+			if (/^(legend|tooltip)Template/.test(key)) {
+				template = document.createElement("div");
+				template.id = this.getLowerFirstCase(RegExp.$1);
+				console.log(template.id)
+				template.style.textAlign = "center";
 
-				this.$chartArea.appendChild(legend);
-				legend = "&lt;div id=\"legend\">&lt;/div>";
+				this.$chartArea.appendChild(template);
+				template = "&lt;div id=\""+ template.id +"\">&lt;/div>";
 			}
 		}
 
@@ -232,9 +239,9 @@ var billboardDemo = {
 
 		// markup
 		if ((index && index === 1) || !index) {
-			code.markup.push("&lt;!-- Markup -->\r\n&lt;div id=\"" + key + "\">&lt;/div>\r\n" + (legend ? legend + "\r\n" : "") + "\r\n");
+			code.markup.push("&lt;!-- Markup -->\r\n&lt;div id=\"" + key + "\">&lt;/div>\r\n" + (template ? template + "\r\n" : "") + "\r\n");
 		} else if (index && index > 1) {
-			code.markup.push("&lt;div id=\"" + key + "\">&lt;/div>\r\n" + (legend ? legend + "\r\n" : "") + "\r\n");
+			code.markup.push("&lt;div id=\"" + key + "\">&lt;/div>\r\n" + (template ? template + "\r\n" : "") + "\r\n");
 		}
 
 		if (index && index > 1) {

demo/demo.js
@@ -2465,6 +2465,35 @@ d3.select(".chart_area")
 				}
 			}
 		},
+		TooltipTemplate: {
+			options: {
+				data: {
+					columns: [
+						["data1", 120, 200, 300, 100, 150, 250],
+						["data2", 250, 129, 217, 240, 315, 225],
+						["data3", 100, 320, 210, 340, 215, 125]
+					]
+				},
+				tooltip: {
+					init: {
+						show: true
+					},
+					doNotHide: true,
+					contents: {
+						bindto: "#tooltip",
+						template: "<ul><li>Index<br>{=TITLE}</li>" +
+								"{{<li class={=CLASS_TOOLTIP_NAME}>" +
+								"<span>{=VALUE}</span><br>" +
+								"<span style=color:{=COLOR}>{=NAME}</span></li>}}</ul>"
+					}
+				}
+			},
+			style: [
+				"#tooltip { font-size: 13px;padding: 0;margin-bottom: 80px; }",
+				"#tooltip li { list-style: none;float: left; padding: 0 10px; }",
+				"#tooltip li span:first-child { font-size: 16px; }"
+			]
+		},
 		LinkedTooltips: [
 			{
 				options: {

demo/simple-sidebar.css
@@ -234,3 +234,7 @@ div.row {
 #MultilinedTitle .bb-title tspan:first-child { font-size: 17px; font-weight: bold; }
 
 #RotatedAxisGroupedBar .base-line line { stroke-width: 3px; stroke: #000; }
+
+#tooltip {font-size: 13px;padding: 0;margin-bottom: 80px;}
+#tooltip li {list-style: none;float: left; padding: 0 10px;}
+#tooltip li span:first-child {font-size: 16px;}

src/config/Options.js
@@ -3211,8 +3211,18 @@ export default class Options {
 			 *  If undefined returned, the row of that value will be skipped.
 			 * @property {Function} [tooltip.position] Set custom position for the tooltip.<br>
 			 *  This option can be used to modify the tooltip position by returning object that has top and left.
-			 * @property {Function} [tooltip.contents] Set custom HTML for the tooltip.<br>
+			 * @property {Function|Object} [tooltip.contents] Set custom HTML for the tooltip.<br>
 			 *  Specified function receives data, defaultTitleFormat, defaultValueFormat and color of the data point to show. If tooltip.grouped is true, data includes multiple data points.
+			 * @property {String|HTMLElement} [tooltip.contents.bindto=undefined] Set CSS selector or element reference to bind tooltip.
+			 * @property {String} [tooltip.contents.template=undefined] Set tooltip's template.
+			 *  - **NOTE:** When is specified, will not be updating tooltip's position.
+			 *  - Within template, below syntax will be replaced using template-like syntax string:
+			 *    - {{ ... }}: the doubly curly brackets indicate loop block for data rows
+			 *    - {=CLASS_TOOLTIP}: default tooltip class name `bb-tooltip`.
+			 *    - {=CLASS_TOOLTIP_NAME}: default tooltip data class name (ex. `bb-tooltip-name-data1`)
+			 *    - {=TITLE}: title value
+			 *    - {=COLOR}: data color
+			 *    - {=VALUE}: data value
 			 * @property {Boolean} [tooltip.init.show=false] Show tooltip at the initialization.
 			 * @property {Number} [tooltip.init.x=0] Set x Axis index to be shown at the initialization.
 			 * @property {Object} [tooltip.init.position={top: "0px",left: "50px"}] Set the position of tooltip at the initialization.
@@ -3241,9 +3251,24 @@ export default class Options {
 			 *      position: function(data, width, height, element) {
 			 *          return {top: 0, left: 0}
   			 *      },
+			 *
   			 *      contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
   			 *          return ... // formatted html as you want
     		 *      },
+			 *
+			 *       // specify tooltip contents using template
+			 *       // - example of HTML returned:
+			 *       // <ul class="bb-tooltip">
+			 *       //   <li class="bb-tooltip-name-data1"><span>250</span><br><span style="color:#00c73c">data1</span></li>
+			 *       //   <li class="bb-tooltip-name-data2"><span>50</span><br><span style="color:#fa7171">data2</span></li>
+			 *       // </ul>
+			 *       contents: {
+			 *      	bindto: "#tooltip",
+			 *      	template: '<ul class={=CLASS_TOOLTIP}>{{' +
+			 *      			'<li class="{=CLASS_TOOLTIP_NAME}"><span>{=VALUE}</span><br>' +
+			 *      			'<span style=color:{=COLOR}>{=NAME}</span></li>' +
+			 *      		'}}</ul>'
+			 *      }
     		 *
     		 *      // sort tooltip data value display in ascending order
     		 *      order: "asc",
@@ -3291,10 +3316,7 @@ export default class Options {
 			tooltip_format_name: undefined,
 			tooltip_format_value: undefined,
 			tooltip_position: undefined,
-			tooltip_contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
-				return this.getTooltipContent ?
-					this.getTooltipContent(d, defaultTitleFormat, defaultValueFormat, color) : "";
-			},
+			tooltip_contents: {},
 			tooltip_init_show: false,
 			tooltip_init_x: 0,
 			tooltip_init_position: {

src/internals/legend.js
@@ -9,7 +9,7 @@ import {
 } from "d3-selection";
 import ChartInternal from "./ChartInternal";
 import CLASS from "../config/classes";
-import {extend, callFn, isDefined, getOption, isEmpty, isFunction, notEmpty} from "./util";
+import {extend, callFn, isDefined, getOption, isEmpty, isFunction, notEmpty, tplProcess} from "./util";
 
 extend(ChartInternal.prototype, {
 	/**
@@ -93,9 +93,10 @@ extend(ChartInternal.prototype, {
 			$$.mapToIds(targets).forEach(v => {
 				const content = isFunction(template) ?
 					template.call($$, v, $$.color(v), $$.api.data(v)[0].values) :
-					template
-						.replace(/{=COLOR}/g, $$.color(v))
-						.replace(/{=TITLE}/g, v);
+					tplProcess(template, {
+						COLOR: $$.color(v),
+						TITLE: v
+					});
 
 				if (content) {
 					ids.push(v);

src/internals/util.js
@@ -316,6 +316,23 @@ const emulateEvent = {
 	}
 };
 
+/**
+ * Process the template  & return bound string
+ * @param {String} tpl Template string
+ * @param {Object} data Data value to be replaced
+ * @return {String}
+ * @private
+ */
+const tplProcess = (tpl, data) => {
+	let res = tpl;
+
+	for (const x in data) {
+		res = res.replace(new RegExp(`{=${x}}`, "g"), data[x]);
+	}
+
+	return res;
+};
+
 export {
 	asHalfPixel,
 	brushEmpty,
@@ -350,5 +367,6 @@ export {
 	notEmpty,
 	sanitise,
 	sortValue,
-	toArray
+	toArray,
+	tplProcess
 };

commit ec13a2c2a05f9f44f716e122a1016735ccd3844d

fix(tooltip): Fix handling on color tile
Fix issue caused by #816.

Ref #816

src/internals/tooltip.js
@@ -202,7 +202,7 @@ extend(ChartInternal.prototype, {
 		return (tplStr || `<table class="{=CLASS_TOOLTIP}"><tbody>
 				{=TITLE}
 				{{<tr class="{=CLASS_TOOLTIP_NAME}">
-					<td class="name"><span style="background-color:{=COLOR}"></span>{=NAME}</td>
+					<td class="name">${this.patterns ? `{=COLOR}` : `<span style="background-color:{=COLOR}"></span>`}{=NAME}</td>
 					<td class="value">{=VALUE}</td>
 				</tr>}}
 			</tbody></table>`)

spec/internals/tooltip-spec.js
@@ -762,5 +762,45 @@ describe("TOOLTIP", function() {
 
 			expect(d3.select("#tooltip").html()).to.be.equal(html);
 		});
+
+		it("set options color.tiles", () => {
+			delete args.data.colors;
+			delete args.tooltip.contents;
+
+			args.color = {
+				tiles: function() {
+					var pattern = d3.select(document.createElementNS(d3.namespaces.svg, "pattern"))
+						.attr("patternUnits", "userSpaceOnUse")
+						.attr("width", "6")
+						.attr("height", "6");
+			
+					var g = pattern
+						.append("g")
+						.attr("fill-rule", "evenodd")
+						.attr("stroke-width", 1)
+						.append("g")
+						.attr("fill", "rgb(255, 127, 14)");
+			
+					g.append("polygon").attr("points", "5 0 6 0 0 6 0 5");
+					g.append("polygon").attr("points", "6 5 6 6 5 6");
+			
+					// Should return an array of SVGPatternElement
+					return [
+						pattern.node()
+					];
+				}
+			}
+		});
+
+		it("check for color tiled tooltip", () => {
+			const id = chart.data().map(v => v.id);
+
+			// when
+			chart.tooltip.show({x:0});
+
+			chart.$.tooltip.selectAll(".name").each(function(d, i) {
+				expect(/^<svg>(.*)<\/svg>$/.test(this.innerHTML.replace(id[i], ""))).to.be.true;
+			});
+		});
 	});
 });