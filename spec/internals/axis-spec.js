naver/billboard.js

commit 57f2e7eaa656ccbebece15bf681dd85ae38dd94d

skip: fix on side-effect (#714)
Correct incorrect data sort, caused by the issue #710 

Close #714

src/internals/util.js
@@ -202,6 +202,25 @@ const getUnique = data => data.filter((v, i, self) => self.indexOf(v) === i);
 const mergeArray = arr => (arr && arr.length ? arr.reduce((p, c) => p.concat(c)) : []);
 
 /**
+ * Sort value
+ * @param {Array} data value to be sorted
+ * @param {Boolean} isAsc true: asc, false: desc
+ * @return {Number|String|Date} sorted date
+ * @private
+ */
+const sortValue = (data, isAsc = true) => {
+	let fn;
+
+	if (data[0] instanceof Date) {
+		fn = isAsc ? (a, b) => a - b : (a, b) => b - a;
+	} else if (!isAsc) {
+		fn = (a, b) => (a > b && -1) || (a < b && 1) || (a === b && 0);
+	}
+
+	return data.concat().sort(fn);
+};
+
+/**
  * Get min/max value
  * @param {String} type 'min' or 'max'
  * @param {Array} data Array data value
@@ -215,9 +234,7 @@ const getMinMax = (type, data) => {
 		if (isNumber(res[0])) {
 			res = Math[type](...res);
 		} else if (res[0] instanceof Date) {
-			const sortFn = type === "min" ? (a, b) => a - b : (a, b) => b - a;
-
-			res = res.sort(sortFn)[0];
+			res = sortValue(res, type === "min")[0];
 		}
 	} else {
 		res = undefined;
@@ -332,5 +349,6 @@ export {
 	mergeArray,
 	notEmpty,
 	sanitise,
+	sortValue,
 	toArray
 };

spec/internals/util-spec.js
@@ -4,7 +4,7 @@
  */
 /* eslint-disable */
 /* global describe, beforeEach, it, expect */
-import {toArray, getCssRules, isArray} from "../../src/internals/util";
+import {toArray, getCssRules, isArray, sortValue} from "../../src/internals/util";
 
 describe("UTIL", function() {
 	describe("toArray", () => {
@@ -25,6 +25,39 @@ describe("UTIL", function() {
 			const rules1 = getCssRules(toArray(document.styleSheets));
 
 			expect(isArray(rules1));
+		});
+	});
+
+
+	describe("sortValue", () => {
+		const value = [1362063600000, 1462063600000, 1562063600000];
+
+		it("sort number", () => {
+			// asc
+			expect(sortValue(value)).to.be.deep.equal(value);
+
+			// desc
+			expect(sortValue(value, false)).to.be.deep.equal(value.concat().reverse());
+		});
+
+		it("sort date", () => {
+			const data = value.map(v => new Date(v));
+
+			// asc
+			expect(sortValue(data)).to.be.deep.equal(data);
+
+			// desc
+			expect(sortValue(data, false)).to.be.deep.equal(data.reverse());
+		});
+
+		it("sort string", () => {
+			const data = ["a", "j", "z"];
+
+			// asc
+			expect(sortValue(data)).to.be.deep.equal(data);
+
+			// desc
+			expect(sortValue(data, false)).to.be.deep.equal(data.reverse());
 		});
 	});
 });

src/data/data.js
@@ -19,7 +19,8 @@ import {
 	isUndefined,
 	isValue,
 	notEmpty,
-	mergeArray
+	mergeArray,
+	sortValue
 } from "../internals/util";
 
 extend(ChartInternal.prototype, {
@@ -427,7 +428,7 @@ extend(ChartInternal.prototype, {
 			xs = $$.isTimeSeries() ? xs.map(x => new Date(+x)) : xs.map(x => +x);
 		}
 
-		return xs.sort((a, b) => (a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN));
+		return sortValue(xs);
 	},
 
 	addHiddenTargetIds(targetIds) {

src/internals/ChartInternal.js
@@ -14,10 +14,9 @@ import {
 	selectAll as d3SelectAll
 } from "d3-selection";
 import {transition as d3Transition} from "d3-transition";
-
 import Axis from "../axis/Axis";
 import CLASS from "../config/classes";
-import {notEmpty, asHalfPixel, getOption, isValue, isArray, isFunction, isString, isNumber, isObject, callFn} from "./util";
+import {notEmpty, asHalfPixel, getOption, isValue, isArray, isFunction, isString, isNumber, isObject, callFn, sortValue} from "./util";
 
 /**
  * Internal chart class.
@@ -229,7 +228,7 @@ export default class ChartInternal {
 		$$.updateScales();
 
 		// Set domains for each scale
-		$$.x.domain($$.getXDomain($$.data.targets).sort());
+		$$.x.domain(sortValue($$.getXDomain($$.data.targets)));
 		$$.y.domain($$.getYDomain($$.data.targets, "y"));
 		$$.y2.domain($$.getYDomain($$.data.targets, "y2"));
 		$$.subX.domain($$.x.domain());

src/internals/domain.js
@@ -3,7 +3,7 @@
  * billboard.js project is licensed under the MIT license
  */
 import ChartInternal from "./ChartInternal";
-import {extend, brushEmpty, getBrushSelection, getMinMax, isDefined, notEmpty, isValue, isObject, isNumber, diffDomain} from "./util";
+import {extend, brushEmpty, getBrushSelection, getMinMax, isDefined, notEmpty, isValue, isObject, isNumber, diffDomain, sortValue} from "./util";
 
 extend(ChartInternal.prototype, {
 	getYDomainMinMax(targets, type) {
@@ -254,7 +254,7 @@ extend(ChartInternal.prototype, {
 		const zoomEnabled = config.zoom_enabled;
 
 		if (withUpdateOrgXDomain) {
-			$$.x.domain(domain || $$.getXDomain(targets).sort());
+			$$.x.domain(domain || sortValue($$.getXDomain(targets)));
 			$$.orgXDomain = $$.x.domain();
 
 			zoomEnabled && $$.zoom.updateScaleExtent();

commit db98b9052e047b48e1ed3a44783c65b9a68dd833

fix(axis): Fix indexed x axis order
Correct domain array ascending order.
Side effect caused by #714

Ref #877

demo/demo.js
@@ -805,6 +805,24 @@ var demos = {
 				}
 			}
 		},
+		IndexedAxis: {
+			options: {
+				data: {
+					x: "x",
+					columns: [
+						["x", 495, 940, 1500, 3000, 4500, 6000, 7500, 9000, 10500, 12000, 13500, 15000],
+						["data", 47.911, 47.915, 48.437, 49.117, 49.583, 50.28, 51.712, 53.103, 54.456, 55.955, 56.752, 56.851]
+					]
+				},
+				axis: {
+					x: {
+						tick: {
+							culling: false
+						}
+					}
+				}
+			}
+		},
 		MultiAxes: [
 			{
 				options: {

src/internals/util.js
@@ -213,8 +213,12 @@ const sortValue = (data, isAsc = true) => {
 
 	if (data[0] instanceof Date) {
 		fn = isAsc ? (a, b) => a - b : (a, b) => b - a;
-	} else if (!isAsc) {
-		fn = (a, b) => (a > b && -1) || (a < b && 1) || (a === b && 0);
+	} else {
+		if (isAsc && data.every(Number)) {
+			fn = (a, b) => a - b;
+		} else if (!isAsc) {
+			fn = (a, b) => (a > b && -1) || (a < b && 1) || (a === b && 0);
+		}
 	}
 
 	return data.concat().sort(fn);

spec/internals/axis-spec.js
@@ -132,6 +132,33 @@ describe("AXIS", function() {
 		});
 	});
 
+	describe("tick values less than 0", () => {
+		before(() => {
+			args = {
+				data: {
+					x: "x",
+					columns: [
+					  	["x", 495, 740, 1500, 3000, 4500, 6000, 7500, 9000, 10500, 12000, 13500, 15000],
+						["data", 47.911, 47.915, 48.437, 49.117, 49.583, 50.28, 51.712, 53.103, 54.456, 55.955, 56.752, 56.851]
+					]
+				},
+				axis: {
+					x: {
+						tick: {
+							culling: false
+						}
+					}
+				}
+			};
+		});
+
+		it("Indexed x axis should scale correct order", () => {
+			chart.$.main.selectAll(".bb-axis-x .tick tspan").each(function(d, i) {
+				expect(+this.textContent).to.be.equal(args.data.columns[0][i + 1]);
+			});
+		});
+	});
+
 	describe("axis y timeseries", () => {
 		before(() => {
 			args = {