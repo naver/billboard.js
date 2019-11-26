194c722afc54220c871ff6bf9b942df135ef8fb1
Travis CI <travis@travis-ci.org>
2019-11-26 01:49:25 +0000

    skip: 1.11.0-nightly-20191126014820 build [skip ci]

--- dist/plugin/billboardjs-plugin-stanford.js
+++ dist/plugin/billboardjs-plugin-stanford.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * http://naver.github.io/billboard.js/
  * 
- * @version 1.10.2-nightly-20191122121335
+ * @version 1.11.0-nightly-20191126014820
  * @requires billboard.js
  * @summary billboard.js plugin
  */
@@ -1527,7 +1527,7 @@ function () {
   }]), Plugin;
 }();
 
-_defineProperty(Plugin_Plugin, "version", "1.10.2-nightly-20191122121335");
+_defineProperty(Plugin_Plugin, "version", "1.11.0-nightly-20191126014820");
 
 
 
