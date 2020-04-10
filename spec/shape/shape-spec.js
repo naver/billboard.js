naver/billboard.js

commit a47a8290b59c579610ba1ffdd4a3916a8f975a71

fix(shape): Fix non-grouped area/bar to be drawn from bottom of x Axis
Make area/bar path draw to not surpass x Axis position, if axis.y/y2.min option is set.

Fix #1302

src/shape/line.js
@@ -449,7 +449,7 @@ extend(ChartInternal.prototype, {
 			getPoints(d, i)[0][1] :
 			yScaleGetter.call($$, d.id)(
 				$$.isAreaRangeType(d) ?
-					$$.getAreaRangeData(d, "high") : 0
+					$$.getAreaRangeData(d, "high") : ($$.getShapeYMin(d.id))
 			));
 		const value1 = (d, i) => ($$.isGrouped(d.id) ?
 			getPoints(d, i)[1][1] :

src/shape/shape.js
@@ -155,6 +155,18 @@ extend(ChartInternal.prototype, {
 	},
 
 	/**
+	 * Get shape based y Axis min value
+	 * @param {String} id Data id
+	 * @return {Number}
+	 * @private
+	 */
+	getShapeYMin(id) {
+		const $$ = this;
+
+		return (!$$.isGrouped(id) && $$.config[`axis_${$$.axis.getId(id)}_min`]) || 0;
+	},
+
+	/**
 	 * Get Shape's offset data
 	 * @param {function(Object): boolean} typeFilter
 	 * @return {{shapeOffsetTargets: ShapeOffsetTarget[], indexMapByTargetId: object}}
@@ -204,7 +216,8 @@ extend(ChartInternal.prototype, {
 		return (d, idx) => {
 			const ind = $$.getIndices(indices, d.id);
 			const scale = isSub ? $$.getSubYScale(d.id) : $$.getYScale(d.id);
-			const y0 = scale(0);
+			const y0 = scale($$.getShapeYMin(d.id));
+
 			const dataXAsNumber = Number(d.x);
 			let offset = y0;
 

spec/shape/shape.bar-spec.js
@@ -516,6 +516,51 @@ describe("SHAPE BAR", () => {
 				expect(this.getAttribute("d")).to.be.equal(expectedPath[i]);
 			});
 		});
+
+		// check non-grouped bar path position
+		const checkBarPathPos = type => {
+			// Get x Axis pos
+			let xAxisYPos = chart.$.main.select(`.${CLASS.axisX} path`).node().getBoundingClientRect();
+
+			xAxisYPos = type === "y" ? xAxisYPos[type] : xAxisYPos[type] + xAxisYPos.width;
+	
+			chart.$.bar.bars.each(function() {
+				const rect = this.getBoundingClientRect();
+			
+				expect(rect[type] + (type === "y" ? rect.height : 0)).to.be.closeTo(xAxisYPos, 1);
+			});
+		}
+
+		it("set options", () => {
+			args = {
+				data: {
+					columns: [
+						["data1", 20000, 35000, 30000]
+					],
+					type: "bar"
+				  },
+				  axis: {
+					y: {
+						min: 10000,
+						padding: {
+							bottom: 0
+						}
+					}
+				}
+			};
+		});
+
+		it("check bar path node position: non rotated Axis", () => {
+			checkBarPathPos("y");
+		});
+
+		it("set option axis.rotated=true", () => {
+			args.axis.rotated = true;
+		});
+
+		it("check bar path node position: rotated Axis", () => {
+			checkBarPathPos("x");
+		});
 	});
 
 	describe("bar sensitivity", () => {

spec/shape/shape.line-spec.js
@@ -239,6 +239,54 @@ describe("SHAPE LINE", () => {
 		});
 	});
 
+	describe("area path generation", () => {
+		before(() => {
+			args = {
+				data: {
+					columns: [
+						["data1", 20000, 35000, 30000]
+					],
+					type: "area"
+				},
+				axis: {
+					y: {
+						min: 10000,
+						padding: {
+							bottom: 0
+						}
+					}
+				}
+			};
+		});
+
+		// check non-grouped bar path position
+		const checkBarPathPos = type => {
+			// Get x Axis pos
+			let xAxisYPos = chart.$.main.select(`.${CLASS.axisX} path`).node().getBoundingClientRect();
+
+			xAxisYPos = type === "y" ? xAxisYPos[type] : xAxisYPos[type] + xAxisYPos.width;
+
+			chart.$.line.areas.each(function() {
+				const rect = this.getBoundingClientRect();
+			
+				expect(rect[type] + (type === "y" ? rect.height : 0)).to.be.closeTo(xAxisYPos, 1);
+			});
+		}
+
+
+		it("check bar path node position: non rotated Axis", () => {
+			checkBarPathPos("y");
+		});
+
+		it("set option axis.rotated=true", () => {
+			args.axis.rotated = true;
+		});
+
+		it("check bar path node position: rotated Axis", () => {
+			checkBarPathPos("x");
+		});
+	});
+
 	describe("area-range type generation", () => {
 		const min = 120;
 		const max = 220;

src/shape/bar.js
@@ -183,7 +183,7 @@ extend(ChartInternal.prototype, {
 		const yScale = isSub ? $$.getSubYScale : $$.getYScale;
 
 		return (d, i) => {
-			const y0 = yScale.call($$, d.id)(0);
+			const y0 = yScale.call($$, d.id)($$.getShapeYMin(d.id));
 			const offset = barOffset(d, i) || y0; // offset is for stacked bar chart
 			const width = isNumber(barW) ? barW : barW[d.id] || barW.width;
 			const posX = barX(d);

commit 00107664a6457a9cedd31f4442600d73a1d781d3

fix(shape): Fix incorrect area/bar rendering
Fix side-effect caused by #1304, making to return y Axis base value based on the current y Axis scale value.
Make sure this to used only non-grouped data and for y Axis min is greater than 0.

Ref #1316

src/shape/line.js
@@ -211,7 +211,7 @@ extend(ChartInternal.prototype, {
 		const yScale = isSub ? $$.getSubYScale : $$.getYScale;
 
 		return (d, i) => {
-			const y0 = yScale.call($$, d.id)(0);
+			const y0 = yScale.call($$, d.id)($$.getShapeYMin(d.id));
 			const offset = lineOffset(d, i) || y0; // offset is for stacked area chart
 			const posX = x(d);
 			let posY = y(d);
@@ -449,7 +449,7 @@ extend(ChartInternal.prototype, {
 			getPoints(d, i)[0][1] :
 			yScaleGetter.call($$, d.id)(
 				$$.isAreaRangeType(d) ?
-					$$.getAreaRangeData(d, "high") : ($$.getShapeYMin(d.id))
+					$$.getAreaRangeData(d, "high") : $$.getShapeYMin(d.id)
 			));
 		const value1 = (d, i) => ($$.isGrouped(d.id) ?
 			getPoints(d, i)[1][1] :
@@ -507,7 +507,7 @@ extend(ChartInternal.prototype, {
 		const yScale = isSub ? $$.getSubYScale : $$.getYScale;
 
 		return function(d, i) {
-			const y0 = yScale.call($$, d.id)(0);
+			const y0 = yScale.call($$, d.id)($$.getShapeYMin(d.id));
 			const offset = areaOffset(d, i) || y0; // offset is for stacked area chart
 			const posX = x(d);
 			let posY = y(d);

src/shape/shape.js
@@ -162,8 +162,9 @@ extend(ChartInternal.prototype, {
 	 */
 	getShapeYMin(id) {
 		const $$ = this;
+		const [yMin] = $$[$$.axis.getId(id)].domain();
 
-		return (!$$.isGrouped(id) && $$.config[`axis_${$$.axis.getId(id)}_min`]) || 0;
+		return !$$.isGrouped(id) && yMin > 0 ? yMin : 0;
 	},
 
 	/**

spec/shape/shape-spec.js
@@ -1,0 +1,89 @@
+/**
+ * Copyright (c) 2017 ~ present NAVER Corp.
+ * billboard.js project is licensed under the MIT license
+ */
+/* eslint-disable */
+/* global describe, beforeEach, it, expect */
+import util from "../assets/util";
+
+describe("SHAPE", () => {
+	let chart;
+	let args;
+
+	beforeEach(() => {
+		chart = util.generate(args);
+	});
+
+	describe("shapes rendering", () => {
+		before(() => {
+			args = {
+				data: {
+					columns: [
+					  ["data1", 100, 200, 300, 400, 500],
+					  ["data2", -100, -200, -300, -400, -500]
+					],
+					types: {
+					  data1: "area-step",
+					  data2: "bar"
+					},
+					axes: {
+					  data1: "y",
+					  data2: "y2"
+					},
+					labels: {
+					  show: true
+					}
+				  },
+				  area: {
+					linearGradient: true
+				  },
+				  axis: {
+					y: {
+					  show: true,
+					  min: -1000,
+					  max: 1000
+					},
+					y2: {
+					  show: true,
+					  min: -1000,
+					  max: 1000
+					}
+				  },
+				  legend: {
+					show: false
+				  }
+			};
+		});
+
+		it("area/bars combination with positive and negative values", () => {
+			const expectedY = 228;
+
+			// check area
+			let rect = chart.$.line.areas.node().getBoundingClientRect();
+
+			expect(rect.y + rect.height).to.be.equal(expectedY);
+
+			// check area data label text position
+			chart.$.text.texts.filter(d => d.id === "data1").each(function() {
+				expect(this.getBoundingClientRect().y).to.be.below(expectedY);
+			});
+			
+			// check bars
+			let height = 0;
+
+			chart.$.bar.bars.each(function() {
+				rect = this.getBoundingClientRect();
+
+				expect(rect.y).to.be.equal(expectedY);
+				expect(rect.height).to.be.above(height);
+
+				height = rect.height;
+			});
+
+			// check bars data label text position
+			chart.$.text.texts.filter(d => d.id === "data2").each(function() {
+				expect(this.getBoundingClientRect().y).to.be.above(expectedY);
+			});
+		});
+	});
+});