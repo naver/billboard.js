naver/billboard.js

commit 01ba388477367c4c1bf7a52083331abbec34b136

fix(grid): Fix focus grid to be maintained on resize
On touch environment, make focus grid line to be maintained on resize(rotate)

Ref #1239

src/internals/ChartInternal.js
@@ -655,9 +655,6 @@ export default class ChartInternal {
 		// update circleY based on updated parameters
 		$$.updateCircleY();
 
-		// xgrid focus
-		$$.updategridFocus();
-
 		// Data empty label positioning and text.
 		config.data_empty_label_text && main.select(`text.${CLASS.text}.${CLASS.empty}`)
 			.attr("x", $$.width / 2)
@@ -837,6 +834,7 @@ export default class ChartInternal {
 			}
 
 			$$.hasType("bar") && list.push($$.redrawBar(bar, isTransition));
+			!flow && list.push($$.updategridFocus());
 		}
 
 		if (!hasArcType || $$.hasType("radar")) {

src/internals/grid.js
@@ -376,19 +376,28 @@ extend(ChartInternal.prototype, {
 	},
 
 	hideGridFocus() {
-		this.main.selectAll(`line.${CLASS.xgridFocus}, line.${CLASS.ygridFocus}`)
+		const $$ = this;
+
+		$$.inputType === "mouse" && $$.main.selectAll(`line.${CLASS.xgridFocus}, line.${CLASS.ygridFocus}`)
 			.style("visibility", "hidden");
 	},
 
 	updategridFocus() {
 		const $$ = this;
-		const isRotated = $$.config.axis_rotated;
 
-		$$.main.select(`line.${CLASS.xgridFocus}`)
-			.attr("x1", isRotated ? 0 : -10)
-			.attr("x2", isRotated ? $$.width : -10)
-			.attr("y1", isRotated ? -10 : 0)
-			.attr("y2", isRotated ? -10 : $$.height);
+		if ($$.inputType === "touch") {
+			const d = $$.grid.select(`line.${CLASS.xgridFocus}`).datum();
+
+			d && $$.showGridFocus([d]);
+		} else {
+			const isRotated = $$.config.axis_rotated;
+
+			$$.main.select(`line.${CLASS.xgridFocus}`)
+				.attr("x1", isRotated ? 0 : -10)
+				.attr("x2", isRotated ? $$.width : -10)
+				.attr("y1", isRotated ? -10 : 0)
+				.attr("y2", isRotated ? -10 : $$.height);
+		}
 	},
 
 	generateGridData(type, scale) {

spec/interactions/interaction-spec.js
@@ -965,6 +965,41 @@ describe("INTERACTION", () => {
 		});
 	});
 
+	describe("check for touch move selection", () => {
+		const selection = [];
+
+		before(() => {
+			args = {
+				data: {	
+					columns: [
+						["data", 3000, 2000, 1000, 4000]
+					]
+				},
+				interaction: {
+					inputType: {
+						touch: true
+					}
+				}
+			};
+		});
+
+		it("x focus grid position & visibility should be maintained after resize", done => {
+			chart.tooltip.show({x:2});
+			chart.resize({width:300});
+
+			setTimeout(() => {
+				const xGridFocus = chart.$.main.select(`.${CLASS.xgridFocus} line`);
+				const x = chart.internal.xx(xGridFocus.datum());
+
+				expect(x).to.be.equal(+xGridFocus.attr("x1"));
+				expect(x).to.be.equal(+xGridFocus.attr("x2"));
+				expect(xGridFocus.style("visibility")).to.be.equal("visible");
+
+				done();
+			}, 300);
+		});
+	});
+
 	describe("check for data.over/out", () => {
 		const spy1 = sinon.spy();
 		const spy2 = sinon.spy();

commit 965bd867b7c1ed9fdf5384cccb39ce2de4b6812e

fix(core): Fix handling transition tasks
- Fix the side-effect caused by #1276
- Make functions added to ‘generateWait()’ to be determined as ‘done’ when return true
- rename ‘.updategridFocus()’ to ‘updateGridFocus()’

Ref #1276
Ref #1343

src/internals/ChartInternal.js
@@ -838,7 +838,7 @@ export default class ChartInternal {
 			}
 
 			$$.hasType("bar") && list.push($$.redrawBar(bar, isTransition));
-			!flow && list.push($$.updategridFocus());
+			!flow && list.push($$.updateGridFocus());
 		}
 
 		if (!hasArcType || $$.hasType("radar")) {
@@ -1200,7 +1200,7 @@ export default class ChartInternal {
 				let done = 0;
 
 				for (let i = 0, t; (t = transitionsToWait[i]); i++) {
-					if (t.empty()) {
+					if (t === true || (t.empty && t.empty())) {
 						done++;
 						continue;
 					}

src/internals/grid.js
@@ -382,12 +382,11 @@ extend(ChartInternal.prototype, {
 			.style("visibility", "hidden");
 	},
 
-	updategridFocus() {
+	updateGridFocus() {
 		const $$ = this;
+		const xgridFocus = $$.grid.select(`line.${CLASS.xgridFocus}`);
 
 		if ($$.inputType === "touch") {
-			const xgridFocus = $$.grid.select(`line.${CLASS.xgridFocus}`);
-
 			if (!xgridFocus.empty()) {
 				const d = xgridFocus.datum();
 
@@ -396,12 +395,16 @@ extend(ChartInternal.prototype, {
 		} else {
 			const isRotated = $$.config.axis_rotated;
 
-			$$.main.select(`line.${CLASS.xgridFocus}`)
+			xgridFocus
 				.attr("x1", isRotated ? 0 : -10)
 				.attr("x2", isRotated ? $$.width : -10)
 				.attr("y1", isRotated ? -10 : 0)
 				.attr("y2", isRotated ? -10 : $$.height);
 		}
+
+		// need to return 'true' as of being pushed to the redraw list
+		// ref: getRedrawList()
+		return true;
 	},
 
 	generateGridData(type, scale) {

src/internals/text.js
@@ -108,18 +108,20 @@ extend(ChartInternal.prototype, {
 		const t = getRandom();
 		const opacityForText = forFlow ? 0 : $$.opacityForText.bind($$);
 
-		return [
-			$$.mainText.each(function(d, i) {
-				const text = d3Select(this);
+		$$.mainText.each(function(d, i) {
+			const text = d3Select(this);
 
-				// do not apply transition for newly added text elements
-				(withTransition && text.attr("x") ? text.transition(t) : text)
-					.attr("x", x.bind(this)(d, i))
-					.attr("y", y.bind(this)(d, i))
-					.style("fill", $$.updateTextColor.bind($$))
-					.style("fill-opacity", opacityForText);
-			})
-		];
+			// do not apply transition for newly added text elements
+			(withTransition && text.attr("x") ? text.transition(t) : text)
+				.attr("x", x.bind(this)(d, i))
+				.attr("y", y.bind(this)(d, i))
+				.style("fill", $$.updateTextColor.bind($$))
+				.style("fill-opacity", opacityForText);
+		});
+
+		// need to return 'true' as of being pushed to the redraw list
+		// ref: getRedrawList()
+		return true;
 	},
 
 	/**

spec/internals/core-spec.js
@@ -131,8 +131,16 @@ describe("CORE", function() {
 			}
 		});
 
-		it("check radar type", () => {
+		it("check radar type", done => {
 			expect(spy.returnValues).to.be.not.empty;
+
+			// when
+			chart.toggle("data1");
+
+			setTimeout(() => {
+				expect(spy.calledTwice).to.be.true;
+				done();
+			}, 300);
 		});
 
 		it("set options data.type='donut'", () => {
@@ -140,7 +148,7 @@ describe("CORE", function() {
 			spy.resetHistory();
 		});
 
-		// Note: Arc types are rendered with transition
+		// Note: Arc types are rendered with tra3nsition
 		it("check donut type", done => {
 			setTimeout(() => {
 				expect(spy.returnValues).to.be.not.empty;
@@ -150,13 +158,15 @@ describe("CORE", function() {
 
 		it("set options data.type='line'", () => {
 			args.data.type = "line";
-			spy.resetHistory();
+			args.onrendered = sinon.spy;
 		});
 
 		// Note: Arc types are rendered with transition
-		it("check donut type", done => {
+		it("check line type", done => {
+			expect(spy.calledOnce).to.be.true;
+
 			setTimeout(() => {
-				expect(spy.returnValues).to.be.not.empty;
+				expect(spy.calledTwice).to.be.true;
 				done();
 			}, 300);
 		});