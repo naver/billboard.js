e0805720ef819a4031c9e19cc4d4294510b6af6e
netil <alberto.park@gmail.com>
2020-07-19 14:49:48 +0000

    skip: 2.0.0-nightly-20200719144903 build [skip ci]

--- dist/billboard.pkgd.js
+++ dist/billboard.pkgd.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  * 
- * @version 2.0.0-nightly-20200717144828
+ * @version 2.0.0-nightly-20200719144903
  * 
  * All-in-one packaged file for ease use of 'billboard.js' with dependant d3.js modules & polyfills.
  * - d3-axis ^1.0.12
@@ -38840,7 +38840,7 @@ var _defaults = {},
    *    bb.version;  // "1.0.0"
    * @memberof bb
    */
-  version: "2.0.0-nightly-20200717144828",
+  version: "2.0.0-nightly-20200719144903",
 
   /**
    * Generate chart
@@ -38968,7 +38968,7 @@ var _defaults = {},
 };
 /**
  * @namespace bb
- * @version 2.0.0-nightly-20200717144828
+ * @version 2.0.0-nightly-20200719144903
  */
 // CONCATENATED MODULE: ./src/index.ts
 /**
