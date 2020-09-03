fa4fd98a817dec9f326bc3b9a77cd9790172d0c8
netil <alberto.park@gmail.com>
2020-09-03 15:22:20 +0000

    skip: 2.0.3-nightly-20200903152116 build [skip ci]

--- dist/billboard.js
+++ dist/billboard.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  * 
- * @version 2.0.3-nightly-20200827151759
+ * @version 2.0.3-nightly-20200903152116
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -16811,7 +16811,7 @@ var _defaults = {},
    *    bb.version;  // "1.0.0"
    * @memberof bb
    */
-  version: "2.0.3-nightly-20200827151759",
+  version: "2.0.3-nightly-20200903152116",
 
   /**
    * Generate chart
@@ -16939,7 +16939,7 @@ var _defaults = {},
 };
 /**
  * @namespace bb
- * @version 2.0.3-nightly-20200827151759
+ * @version 2.0.3-nightly-20200903152116
  */
 // CONCATENATED MODULE: ./src/index.ts
 /**
--- dist/billboard.pkgd.js
+++ dist/billboard.pkgd.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  * 
- * @version 2.0.3-nightly-20200827151759
+ * @version 2.0.3-nightly-20200903152116
  * 
  * All-in-one packaged file for ease use of 'billboard.js' with dependant d3.js modules & polyfills.
  * - d3-axis ^1.0.12
@@ -38936,7 +38936,7 @@ var _defaults = {},
    *    bb.version;  // "1.0.0"
    * @memberof bb
    */
-  version: "2.0.3-nightly-20200827151759",
+  version: "2.0.3-nightly-20200903152116",
 
   /**
    * Generate chart
@@ -39064,7 +39064,7 @@ var _defaults = {},
 };
 /**
  * @namespace bb
- * @version 2.0.3-nightly-20200827151759
+ * @version 2.0.3-nightly-20200903152116
  */
 // CONCATENATED MODULE: ./src/index.ts
 /**
