5dde678a511cb1500735d7041451fae15a0a0189
netil <alberto.park@gmail.com>
2020-10-29 17:02:20 +0000

    skip: 2.1.3-nightly-20201029170117 build [skip ci]

--- dist/billboard.js
+++ dist/billboard.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  * 
- * @version 2.1.3-nightly-20201028171105
+ * @version 2.1.3-nightly-20201029170117
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -16929,7 +16929,7 @@ var _defaults = {},
    *    bb.version;  // "1.0.0"
    * @memberof bb
    */
-  version: "2.1.3-nightly-20201028171105",
+  version: "2.1.3-nightly-20201029170117",
 
   /**
    * Generate chart
@@ -17057,7 +17057,7 @@ var _defaults = {},
 };
 /**
  * @namespace bb
- * @version 2.1.3-nightly-20201028171105
+ * @version 2.1.3-nightly-20201029170117
  */
 // CONCATENATED MODULE: ./src/index.ts
 /**
