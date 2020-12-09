07f6e5ac3adf571cefaafca1e700cce609e30b7c
Jae Sung Park <alberto.park@gmail.com>
2020-12-09 18:28:56 +0900

    chore(browser): udpate retrive of global
    Update the way of checking global and try..catch block to prevent undefined error.
    
    Ref #1778

--- src/module/browser.ts
+++ src/module/browser.ts
@@ -10,11 +10,19 @@
 export {win as window, doc as document};
 
 const win = (() => {
-	const def = o => typeof o !== "undefined" && o;
+	const def = o => typeof o === "object" && o !== null && o.Object === Object && o;
 
 	// Prioritize referencing Node.js global first to prevent refence error
 	// https://github.com/naver/billboard.js/issues/1778
-	return def(global) || def(globalThis) || def(self) || def(window) || Function("return this")();
+	let freeGlobal = def(global);
+
+	try {
+		if (!freeGlobal) {
+			freeGlobal = def(globalThis) || def(self) || def(window);
+		}
+	} catch (e) {}
+
+	return freeGlobal || Function("return this")();
 })();
 /* eslint-enable no-new-func, no-undef */
 
