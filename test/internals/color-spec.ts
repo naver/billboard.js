naver/billboard.js

commit 86ef214a2daf22fb7e4058644776c9d1101e7350

fix(color): fix color callback mismatch
Fix insconsistent param type for color callback function

Fix #1847

test/internals/tooltip-spec.ts
@@ -1186,4 +1186,34 @@ describe("TOOLTIP", function() {
 			chart.$.chart.style("margin-left", null);
 		});
 	});
+
+	describe("Tooltip color", () => {
+		const c = {data1: "yellow", data2: "green"};
+
+		before(() => {
+			args = {
+				data: {
+					columns: [
+						["data1", 30],
+						["data2", 50]
+					],
+					color: (color, d) => c[d.id] || color
+				}
+			};
+		});
+
+		it("should apply correct color values", () => {			
+			chart.$.circles.each(function(d) {
+				expect(this.style.fill).to.be.equal(c[d.id]);
+			});
+
+			// when
+			chart.tooltip.show({index:0});
+
+
+			["data1", "data2"].forEach(v => {
+				expect(chart.$.tooltip.selectAll(`.${CLASS.tooltipName}-${v} .name span`).style("background-color")).to.be.equal(c[v]);
+			});
+		});
+	});
 });

src/ChartInternal/data/data.ts
@@ -928,5 +928,17 @@ export default {
 			(isObject(d.value) && ("z" in d.value || "y" in d.value)) ||
 			(isArray(d.value) && d.value.length === 2)
 		);
+	},
+
+	/**
+	 * Get data object by id
+	 * @param {string} id data id
+	 * @returns {object}
+	 * @private
+	 */
+	getDataById(id: string) {
+		const d = this.cache.get(id) || this.api.data(id);
+
+		return isArray(d) ? d[0] : d;
 	}
 };

src/ChartInternal/internals/legend.ts
@@ -600,6 +600,14 @@ export default {
 			.attr("x", isLegendRightOrInset ? xForLegendRect : pos)
 			.attr("y", isLegendRightOrInset ? pos : yForLegendRect);
 
+		const getColor = id => {
+			const data = $$.getDataById(id);
+
+			return $$.levelColor ?
+				$$.levelColor(data.values[0].value) :
+				$$.color(data);
+		};
+
 		const usePoint = config.legend_usePoint;
 
 		if (usePoint) {
@@ -620,7 +628,7 @@ export default {
 				return document.createElementNS(d3Namespaces.svg, ("hasValidPointType" in $$) && $$.hasValidPointType(point) ? point : "use");
 			})
 				.attr("class", CLASS.legendItemPoint)
-				.style("fill", d => $$.color(d))
+				.style("fill", getColor)
 				.style("pointer-events", "none")
 				.attr("href", (data, idx, selection) => {
 					const node = selection[idx];
@@ -631,7 +639,7 @@ export default {
 		} else {
 			l.append("line")
 				.attr("class", CLASS.legendItemTile)
-				.style("stroke", $$.color)
+				.style("stroke", getColor)
 				.style("pointer-events", "none")
 				.attr("x1", isLegendRightOrInset ? x1ForLegendTile : pos)
 				.attr("y1", isLegendRightOrInset ? pos : yForLegendTile)
@@ -668,7 +676,6 @@ export default {
 			.attr("height", id => heights[id])
 			.attr("x", xForLegendRect)
 			.attr("y", yForLegendRect);
-
 
 		if (usePoint) {
 			const tiles = legend.selectAll(`.${CLASS.legendItemPoint}`)
@@ -714,7 +721,7 @@ export default {
 				.data(targetIdz);
 
 			(withTransition ? tiles.transition() : tiles)
-				.style("stroke", $$.levelColor ? id => $$.levelColor($$.cache.get(id).values[0].value) : $$.color)
+				.style("stroke", getColor)
 				.attr("x1", x1ForLegendTile)
 				.attr("y1", yForLegendTile)
 				.attr("x2", x2ForLegendTile)

test/internals/legend-spec.ts
@@ -485,7 +485,8 @@ describe("LEGEND", () => {
 						["padded2", 90],
 						["padded3", 50],
 						["padded4", 20]
-					]
+					],
+					type: "gauge",
 				},
 				color: {
 					pattern: ["#FF0000", "#F97600", "#F6C600", "#60B044"],
@@ -493,10 +494,9 @@ describe("LEGEND", () => {
 						values: [30, 80, 95]
 					}
 				},
-				type: "gauge",
 				gauge: {
 					type: "multi"
-				},
+				}
 			};
 		});
 

commit 8de3c8198ff04616f622219f725d41a4c693e118

fix(arc): fix setting color value
Fix color value setting caused by the #1857

Ref #1847

src/ChartInternal/shape/arc.ts
@@ -628,7 +628,7 @@ export default {
 					// update data's color
 					config.data_colors[d.data.id] = color;
 				} else {
-					color = $$.color(d.data.id);
+					color = $$.color(d.data);
 				}
 
 				return color;

test/internals/color-spec.ts
@@ -411,4 +411,46 @@ describe("COLOR", () => {
 			}, 1000);
 		});
 	});
+
+	describe("Tooltip color", () => {
+		const c = {data1: "yellow", data2: "green"};
+
+		before(() => {
+			args = {
+				data: {
+					columns: [
+						["data1", 30],
+						["data2", 50]
+					],
+					color: (color, d) => c[d.id] || color
+				}
+			};
+		});
+
+		it("should apply correct color values", () => {			
+			chart.$.circles.each(function(d) {
+				expect(this.style.fill).to.be.equal(c[d.id]);
+			});
+
+			// when
+			chart.tooltip.show({index:0});
+
+
+			["data1", "data2"].forEach(v => {
+				expect(chart.$.tooltip.selectAll(`.${CLASS.tooltipName}-${v} .name span`).style("background-color")).to.be.equal(c[v]);
+			});
+		});
+
+		it("set option data.type='donut'", () => {
+			args.data.type = "donut";
+		});
+
+		it("arc should apply correct color values", () => {
+			chart.$.arc.selectAll("path").each(function(d) {
+				expect(this.style.fill).to.be.equal(
+					chart.internal.$el.legend.select(`.${CLASS.legendItem}-${d.data.id} line`).style("stroke")
+				);
+			});
+		})
+	});
 });

test/internals/tooltip-spec.ts
@@ -1186,34 +1186,4 @@ describe("TOOLTIP", function() {
 			chart.$.chart.style("margin-left", null);
 		});
 	});
-
-	describe("Tooltip color", () => {
-		const c = {data1: "yellow", data2: "green"};
-
-		before(() => {
-			args = {
-				data: {
-					columns: [
-						["data1", 30],
-						["data2", 50]
-					],
-					color: (color, d) => c[d.id] || color
-				}
-			};
-		});
-
-		it("should apply correct color values", () => {			
-			chart.$.circles.each(function(d) {
-				expect(this.style.fill).to.be.equal(c[d.id]);
-			});
-
-			// when
-			chart.tooltip.show({index:0});
-
-
-			["data1", "data2"].forEach(v => {
-				expect(chart.$.tooltip.selectAll(`.${CLASS.tooltipName}-${v} .name span`).style("background-color")).to.be.equal(c[v]);
-			});
-		});
-	});
 });