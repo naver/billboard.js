naver/billboard.js

commit 6ff9aec01d831c8fe2449da6b87121281829f209

fix(point): fix error when data has spaced name
Convert id value using .getTargetSelectorSuffix() to prevent
node query selection error.

Fix #1739

src/ChartInternal/shape/point.ts
@@ -446,7 +446,8 @@ export default {
 
 		return function(method, context, ...args) {
 			return function(d) {
-				const id: string = d.id || (d.data && d.data.id) || d;
+				const id: string = $$.getTargetSelectorSuffix(d.id || (d.data && d.data.id) || d);
+
 				const element = d3Select(this);
 
 				ids.indexOf(id) < 0 && ids.push(id);

test/shape/point-spec.ts
@@ -81,7 +81,7 @@ describe("SHAPE POINT", () => {
 				data: {
 					columns: [
 						["data1", 30, 200, 100, 400, -150, 250],
-						["data2", 50, 20, 10, 40, 15, 25],
+						["data 2", 50, 20, 10, 40, 15, 25],
 						["data3", -150, 120, 110, 140, 115, 125]
 					]
 				},

commit 7845a765052de90352de127d304c0caf8f8a7a79

fix(point): fix usePoint defs id value
Incorrect id value was set for custom data point.
Side-effect caused by 6ff9aec0

Ref #1887

src/ChartInternal/shape/point.ts
@@ -446,7 +446,6 @@ export default {
 		return function(method, context, ...args) {
 			return function(d) {
 				const id: string = $$.getTargetSelectorSuffix(d.id || (d.data && d.data.id) || d);
-
 				const element = d3Select(this);
 
 				ids.indexOf(id) < 0 && ids.push(id);
@@ -456,7 +455,7 @@ export default {
 				if ($$.hasValidPointType(point)) {
 					point = $$[point];
 				} else if (!$$.hasValidPointDrawMethods(point)) {
-					const pointId = `${datetimeId}-point-${id}`;
+					const pointId = `${datetimeId}-point${id}`;
 					const pointFromDefs = $$.pointFromDefs(pointId);
 
 					if (pointFromDefs.size() < 1) {

test/internals/legend-spec.ts
@@ -461,15 +461,20 @@ describe("LEGEND", () => {
 		});
 
 		it("should render custom points in legend", () => {
-			const nodes = chart.internal.$el.svg.selectAll(`.${CLASS.legendItem} .${CLASS.legendItemPoint}`);
+			const {$el} = chart.internal;
+			const nodes = $el.svg.selectAll(`.${CLASS.legendItem} .${CLASS.legendItemPoint}`);
+			const pointTagName = ["circle", "rect", "use", "circle"];
 
-			nodes.each((data, idx, selection) => {
+			nodes.each(function(data, idx, selection) {
 				const node = selection[idx];
 				const nodeName = node.nodeName.toLowerCase();
-				const expected = (idx === 0 || idx === 3) ?
-				  "circle" : (idx === 1) ? "rect" : (idx === 2) ? "use" : "";
 
-				expect(nodeName).to.be.equal(expected);
+				// check if referenced defs node exists
+				if (pointTagName[idx] === "use") {					
+					expect($el.defs.select(this.getAttribute("href")).size()).to.be.equal(1);
+				}
+
+				expect(nodeName).to.be.equal(pointTagName[idx]);
 			});
 
 			expect(nodes.size()).to.be.equal(chart.data().length);