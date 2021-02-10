naver/billboard.js

commit 9d6e1e085e67a9b4d05d28c726eb451c1aa691e1

fix(area): Fix dynamic area loading
Make sure to pass <g class=bb-chart-line> element for area node generation.

Fix #1547

src/ChartInternal/shape/line.ts
@@ -18,7 +18,7 @@ export default {
 
 	updateTargetsForLine(t): void {
 		const $$ = this;
-		const {$el: {line, main}} = $$;
+		const {$el: {area, line, main}} = $$;
 		const classChartLine = $$.classChartLine.bind($$);
 		const classLines = $$.classLines.bind($$);
 		const classFocus = $$.classFocus.bind($$);
@@ -44,12 +44,11 @@ export default {
 			.attr("class", classLines);
 
 		// Areas
-		$$.hasTypeOf("Area") && $$.initArea(mainLineEnter);
+		if ($$.hasTypeOf("Area") && !area) {
+			$$.initArea(mainLineEnter.empty() ? mainLineUpdate : mainLineEnter);
+		}
 
 		$$.updateTargetForCircle(targets, mainLineEnter);
-
-		// MEMO: can not keep same color...
-		// mainLineUpdate.exit().remove();
 	},
 
 	updateLine(durationForExit): void {

test/api/load-spec.ts
@@ -8,6 +8,7 @@ import {select as d3Select} from "d3-selection";
 import {format as d3Format} from "d3-format";
 import CLASS from "../../src/config/classes";
 import util from "../assets/util";
+import { area } from "../../src/config/resolver/shape";
 
 describe("API load", function() {
 	let chart;
@@ -461,6 +462,67 @@ describe("API load", function() {
 					done();
 				}
 			});
+		});
+	});
+
+	describe("different type loading", () => {
+		before(() => {
+			args = {
+				data: {
+					columns: [
+						["data1", 30, 200, 100],
+					]
+				}
+			};
+		});
+
+		it("check 'line' -> 'area' type loading", done => {
+			const {areas} = chart.$.line;
+
+			expect(areas).to.be.null;
+
+			setTimeout(() => {
+				chart.load({
+					columns: [
+						["data1", 100, 200, 300]
+					],
+					type: "area",
+					done: function() {
+						const {areas} = this.$.line;
+
+						expect(areas && !areas.empty()).to.be.true;
+						done();
+					}
+				});
+			}, 500);
+		});
+
+		it("set options data.type='area'", () => {
+			args.data.type = "area";
+		});
+
+		it("check 'area' -> 'area-spline' type loading", done => {
+			const {areas} = chart.$.line;
+
+			expect(areas && !areas.empty()).to.be.true;
+
+			setTimeout(() => {
+				chart.load({
+					columns: [
+						["data1", 100, 200, 300]
+					],
+					type: "area-spline",
+					done: function() {
+						const {areas} = this.$.line;
+
+						expect(areas && !areas.empty()).to.be.true;
+
+						// check for duplicated node appends
+						expect(chart.$.main.selectAll(`.${CLASS.areas}`).size()).to.be.equal(1);
+						done();
+					}
+				});
+			}, 500);
 		});
 	});
 });

src/ChartInternal/shape/area.ts
@@ -8,11 +8,11 @@ import CLASS from "../../config/classes";
 import {getRandom, isFunction} from "../../module/util";
 
 export default {
-	initArea(mainLineEnter): void {
+	initArea(mainLine): void {
 		const $$ = this;
 
-		mainLineEnter
-			.append("g")
+		mainLine
+			.insert("g", `.${CLASS.circles}`)
 			.attr("class", $$.classAreas.bind($$));
 	},
 

commit 31b97b49bdbc59eda8171e68f6c26c9466ab0b90

fix(api): fix loading area-line-range
Fix side-effect caused by 9d6e1e0.

Ref #1938

src/ChartInternal/shape/line.ts
@@ -44,8 +44,8 @@ export default {
 			.attr("class", classLines);
 
 		// Areas
-		if ($$.hasTypeOf("Area") && !area) {
-			$$.initArea(mainLineEnter.empty() ? mainLineUpdate : mainLineEnter);
+		if ($$.hasTypeOf("Area")) {
+			$$.initArea(!area && mainLineEnter.empty() ? mainLineUpdate : mainLineEnter);
 		}
 
 		$$.updateTargetForCircle(targets, mainLineEnter);

test/api/load-spec.ts
@@ -577,6 +577,55 @@ describe("API load", function() {
 		});
 	});
 
+	describe("area-line-range type loading", () => {
+		before(() => {
+			args = {
+				data: {
+					x: "x",
+					columns: [
+						["x", "2013-01-01", "2013-01-02", "2013-01-03", "2013-01-04", "2013-01-05", "2013-01-06"],
+						["data1",
+							[150, 140, 110],
+							[155, 130, 115],
+							[160, 135, 120],
+							[135, 120, 110],
+							[180, 150, 130],
+							[199, 160, 125]
+						],
+						["data2", 130, 340, 200, 500, 250, 350]
+					],
+					types: {
+					  data1: "area-line-range"
+					}
+				},
+				axis: {
+					x: {
+					  type: "timeseries",
+					  tick: {
+						format: "%Y-%m-%d"
+					  }
+					}
+				}
+			}
+		});
+
+		it("should render range area for newly loaded data", done => {
+			chart.load({
+				columns: [
+					["data3", [220, 215, 205], [240, 225, 215], [260, 235, 225], [280, 245, 235], [270, 255, 225], [240, 225, 215]],
+				],
+				types: {
+					data3: "area-spline-range"
+				},
+				done: function() {
+					expect(this.$.line.areas.filter(`.${CLASS.area}-data3`).size()).to.be.equal(1);
+
+					done();
+				}
+			});
+		});
+	});
+
 	describe("should handle correct event rect lengths", () => {
 		before(() => {
 			args = {