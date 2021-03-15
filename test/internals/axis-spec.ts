ef6b48a3d39137a8f52b0809cffe7ba5cb7a8abe
Jae Sung Park <alberto.park@gmail.com>
2021-03-15 10:14:12 +0900

    fix(axis): fix axis.x.max error throw
    Bind the context correctly when 'parseDate' is called
    within .getXDomainMinMax()
    
    Ref #1981

--- test/internals/axis-spec.ts
+++ test/internals/axis-spec.ts
@@ -2453,6 +2453,31 @@ describe("AXIS", function() {
 				}
 			});
 		});
+
+		it("the use of axis.x.max option, shouldn't throw error", () => {
+			const args = {
+				data: {
+					x: "x",
+					columns: [
+					["x", "2013-01-01", "2013-01-02", "2013-01-03", "2013-01-04", "2013-01-05", "2013-01-06"],
+					["data1", 30, 200, 100, 400, 150, 250],
+					["data2", 130, 340, 200, 500, 250, 350]
+					],
+					type: "line", 
+				},
+				axis: {
+					x: {
+						max: "2013-01-04",
+						type: "timeseries",
+						tick: {
+							format: "%Y-%m-%d"
+						}
+					}
+				}
+			};
+
+			expect(bb.generate(args)).to.not.throw;
+		});
 	});
 
 	describe("x Axis tick width size", () => {
