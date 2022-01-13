8adfb7c90d63c584a1692ed3d9fd2de73e969ed9
netil <alberto.park@gmail.com>
2022-01-13 00:47:00 +0000

    skip: 3.2.2-nightly-20220113004559 build [skip ci]

--- dist-esm/billboard.js
+++ dist-esm/billboard.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  * 
- * @version 3.2.2-nightly-20220112004611
+ * @version 3.2.2-nightly-20220113004559
 */
 import { timeParse, utcParse, timeFormat, utcFormat } from 'd3-time-format';
 import { pointer, select, namespaces, selectAll } from 'd3-selection';
@@ -20287,7 +20287,7 @@ var zoomModule = function () {
 var defaults = {};
 /**
  * @namespace bb
- * @version 3.2.2-nightly-20220112004611
+ * @version 3.2.2-nightly-20220113004559
  */
 var bb = {
     /**
@@ -20297,7 +20297,7 @@ var bb = {
      *    bb.version;  // "1.0.0"
      * @memberof bb
      */
-    version: "3.2.2-nightly-20220112004611",
+    version: "3.2.2-nightly-20220113004559",
     /**
      * Generate chart
      * - **NOTE:** Bear in mind for the possiblity of ***throwing an error***, during the generation when:
--- dist/billboard.pkgd.min.js
+++ dist/billboard.pkgd.min.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  *
- * @version 3.2.2-nightly-20220112004611
+ * @version 3.2.2-nightly-20220113004559
  *
  * All-in-one packaged file for ease use of 'billboard.js' with dependant d3.js modules & polyfills.
  * - d3-axis ^3.0.0
