e77ff4e11d3e7a80b9d6a1a4119987daa1cada0e
netil <alberto.park@gmail.com>
2023-04-08 00:48:04 +0000

    skip: 3.7.5-nightly-20230408004638 build [skip ci]

--- dist-esm/billboard.js
+++ dist-esm/billboard.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  * 
- * @version 3.7.5-nightly-20230405004622
+ * @version 3.7.5-nightly-20230408004638
 */
 import { timeParse, utcParse, timeFormat, utcFormat } from 'd3-time-format';
 import { pointer, select, namespaces, selectAll } from 'd3-selection';
@@ -22264,7 +22264,7 @@ var zoomModule = function () {
 var defaults = {};
 /**
  * @namespace bb
- * @version 3.7.5-nightly-20230405004622
+ * @version 3.7.5-nightly-20230408004638
  */
 var bb = {
     /**
@@ -22274,7 +22274,7 @@ var bb = {
      *    bb.version;  // "1.0.0"
      * @memberof bb
      */
-    version: "3.7.5-nightly-20230405004622",
+    version: "3.7.5-nightly-20230408004638",
     /**
      * Generate chart
      * - **NOTE:** Bear in mind for the possiblity of ***throwing an error***, during the generation when:
