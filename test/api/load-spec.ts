naver/billboard.js

commit 13b9da7b9e2ad4ef0f2b4d8ad3d0f4770a1e79e4

fix(api): fix tooltip work for .load() with xs key
when data initializes with empty data, do not initialize eventRect
and initializes when data is loaded dynamically.

Fix #3194

src/ChartInternal/interactions/eventrect.ts
@@ -32,7 +32,9 @@ export default {
 
 		if ($el.eventRect) {
 			$$.updateEventRect($el.eventRect, true);
-		} else {
+
+		// do not initialize eventRect when data is empty
+		} else if ($$.data.targets.length) {
 			const eventRects = $$.$el.main.select(`.${$EVENT.eventRects}`)
 				.style("cursor", config.zoom_enabled && config.zoom_type !== "drag" ? (
 					config.axis_rotated ? "ns-resize" : "ew-resize"
@@ -61,6 +63,9 @@ export default {
 			if ($$.state.inputType === "touch" && !$el.svg.on("touchstart.eventRect") && !$$.hasArcType()) {
 				$$.bindTouchOnEventRect(isMultipleX);
 			}
+
+			// when initilazed with empty data and data loaded later, need to update eventRect
+			state.rendered && $$.updateEventRect($el.eventRect, true);
 		}
 
 		if (!isMultipleX) {

test/api/load-spec.ts
@@ -513,6 +513,45 @@ describe("API load", function() {
 				}
 			});
 		});
+
+		it("set options: initialize with empty data", () => {
+			args = {
+				data: {
+					columns: [],
+					type: "area"
+				}
+			};
+		});
+
+		it("check for correct event binding", done => {
+			setTimeout(() => {
+				chart.load({
+					xs: {
+						data: 'dataX'
+					},
+					columns: [
+						["data", 300, 350, 300, 200, 50, 300],
+						["dataX", 1, 2, 3, 4, 5, 6],
+					],
+					done: function() {
+						this.tooltip.show({
+							data: {
+								x: 3,
+								id: "data",
+								value: 300
+							}
+						});
+
+						const {tooltip} = this.$;
+						
+						expect(tooltip.select(".name").text()).to.be.equal("data");
+						expect(+tooltip.select(".value").text()).to.be.equal(300);
+
+						done();
+					}
+				});
+			  }, 1000);
+		});
 	});
 
 	describe("different type loading", () => {

commit 90dbf82bb079405a1d5879405873e993efdb80d3

fix(api): fix classname setting for  eventRect element
Fix the side effect caused by #3203.
Make sure to set proper class name for eventRect element.

Ref 3218

src/ChartInternal/interactions/eventrect.ts
@@ -208,8 +208,8 @@ export default {
 				.attr("height", height);
 
 			// only for init
-			if (!rendered) {
-				rectElement.attr("class", $EVENT.eventRect);
+			if (!rendered || force) {
+				rectElement.classed($EVENT.eventRect, true);
 			}
 		}
 

test/api/load-spec.ts
@@ -6,7 +6,7 @@
 import {expect} from "chai";
 import {select as d3Select} from "d3-selection";
 import {format as d3Format} from "d3-format";
-import {$AREA, $AXIS, $COMMON, $CIRCLE, $LEGEND, $LINE} from "../../src/config/classes";
+import {$AREA, $AXIS, $COMMON, $CIRCLE, $EVENT, $LEGEND, $LINE} from "../../src/config/classes";
 import util from "../assets/util";
 
 describe("API load", function() {
@@ -519,6 +519,9 @@ describe("API load", function() {
 				data: {
 					columns: [],
 					type: "area"
+				},
+				background: {
+					color: "red"
 				}
 			};
 		});
@@ -534,6 +537,8 @@ describe("API load", function() {
 						["dataX", 1, 2, 3, 4, 5, 6],
 					],
 					done: function() {
+						expect(this.internal.$el.eventRect.classed($EVENT.eventRect)).to.be.true;						
+
 						this.tooltip.show({
 							data: {
 								x: 3,
@@ -552,6 +557,8 @@ describe("API load", function() {
 				});
 			  }, 1000);
 		});
+
+
 	});
 
 	describe("different type loading", () => {