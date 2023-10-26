naver/billboard.js

commit 041e144a57cb7d874a49d834abc85d0bd87747f4

fix(tooltip): Fix wrong tooltip position with padding option
Reflect padding top/left option on tooltip positioning.

Fix #3473

src/ChartInternal/internals/size.ts
@@ -4,7 +4,7 @@
  */
 import {document} from "../../module/browser";
 import {$AXIS, $SUBCHART} from "../../config/classes";
-import {ceil10, capitalize, isNumber, isEmpty, isUndefined} from "../../module/util";
+import {ceil10, capitalize, isNumber, isEmpty, isString, isUndefined} from "../../module/util";
 
 export default {
 	/**
@@ -87,13 +87,26 @@ export default {
 	getSvgLeft(withoutRecompute?: boolean): number {
 		const $$ = this;
 		const {config, $el} = $$;
-		const hasLeftAxisRect = config.axis_rotated || (!config.axis_rotated && !config.axis_y_inner);
-		const leftAxisClass = config.axis_rotated ? $AXIS.axisX : $AXIS.axisY;
+		const isRotated = config.axis_rotated;
+		const hasLeftAxisRect = isRotated || (!isRotated && !config.axis_y_inner);
+		const leftAxisClass = isRotated ? $AXIS.axisX : $AXIS.axisY;
 		const leftAxis = $el.main.select(`.${leftAxisClass}`).node();
+		const leftLabel = config[`axis_${isRotated ? "x" : "y"}_label`];
+		let labelWidth = 0;
+
+		// if axis label position set to inner, exclude from the value
+		if (isString(leftLabel) || isString(leftLabel.text) || /^inner-/.test(leftLabel?.position)) {
+			const label = $el.main.select(`.${leftAxisClass}-label`);
+
+			if (!label.empty()) {
+				labelWidth = label.node().getBoundingClientRect().left;
+			}
+		}
+
 		const svgRect = leftAxis && hasLeftAxisRect ? leftAxis.getBoundingClientRect() : {right: 0};
-		const chartRect = $el.chart.node().getBoundingClientRect();
+		const chartRectLeft = $el.chart.node().getBoundingClientRect().left + labelWidth;
 		const hasArc = $$.hasArcType();
-		const svgLeft = svgRect.right - chartRect.left -
+		const svgLeft = svgRect.right - chartRectLeft -
 			(hasArc ? 0 : $$.getCurrentPaddingByDirection("left", withoutRecompute));
 
 		return svgLeft > 0 ? svgLeft : 0;
@@ -204,7 +217,7 @@ export default {
 				padding += isRotated ? (
 					!isFitPadding && isUndefined(paddingOption) ? 10 : 2
 				) : !isAxisShow || isAxisInner ? (isFitPadding ? 2 : 1) : 0;
-			} else if (type === "left" && isRotated) {
+			} else if (type === "left" && isRotated && isUndefined(paddingOption)) {
 				padding = !config.axis_x_show ?
 					1 : (isFitPadding ? axisSize : Math.max(axisSize, 40));
 			}

src/ChartInternal/internals/tooltip.ts
@@ -330,34 +330,39 @@ export default {
 		const hasGauge = $$.hasType("gauge") && !config.gauge_fullCircle;
 		const hasTreemap = state.hasTreemap;
 		const isRotated = config.axis_rotated;
+		const hasArcType = $$.hasArcType();
 		const svgLeft = $$.getSvgLeft(true);
 		let chartRight = svgLeft + current.width - $$.getCurrentPaddingByDirection("right");
-		const chartLeft = $$.getCurrentPaddingByDirection("left", true);
 		const size = 20;
 		let {x, y} = currPos;
 
 		// Determine tooltip position
-		if ($$.hasArcType()) {
+		if (hasArcType) {
 			const raw = inputType === "touch" || $$.hasType("radar");
 
 			if (!raw) {
-				y += hasGauge ? height : height / 2;
 				x += (width - (isLegendRight ? $$.getLegendWidth() : 0)) / 2;
+				y += hasGauge ? height : height / 2;
 			}
 		} else if (!hasTreemap) {
+			const padding = {
+				top: $$.getCurrentPaddingByDirection("top", true),
+				left: $$.getCurrentPaddingByDirection("left", true)
+			};
+
 			if (isRotated) {
-				y = currPos.xAxis + size;
-				x += svgLeft;
+				x += svgLeft + padding.left + size;
+				y = padding.top + currPos.xAxis + size;
 				chartRight -= svgLeft;
 			} else {
-				y -= 5;
-				x = svgLeft + chartLeft + size + (scale.zoom ? x : currPos.xAxis);
+				x = svgLeft + padding.left + size + (scale.zoom ? x : currPos.xAxis);
+				y += padding.top - 5;
 			}
 		}
 
 		// when tooltip left + tWidth > chart's width
 		if ((x + tWidth + 15) > chartRight) {
-			x -= isRotated ? tWidth - chartLeft : tWidth + (hasTreemap ? 0 : chartLeft);
+			x -= tWidth + (hasTreemap || hasArcType ? 0 : (isRotated ? size * 2 : 38));
 		}
 
 		if (y + tHeight > current.height) {

test/internals/text-spec.ts
@@ -838,7 +838,7 @@ describe("TEXT", () => {
 				});
 
 				it("should locate labels above each data point", () => {
-					const expectedXs = [72, 537, 72, 527]; // 72.50132230092231
+					const expectedXs = [72, 527, 72, 527]; // 72.50132230092231
 					const expectedYs = [9, 157, 305, 434];
 
 					chart.$.main.selectAll(`.${$TEXT.texts}-data1 text`)

test/internals/tooltip-spec.ts
@@ -11,7 +11,7 @@ import {
 	namespaces as d3Namespaces
 } from "d3-selection";
 import util from "../assets/util";
-import {$SHAPE, $TOOLTIP} from "../../src/config/classes";
+import {$CIRCLE, $SHAPE, $TOOLTIP} from "../../src/config/classes";
 import {isNumber, isUndefined, isString} from "../../src/module/util";
 
 describe("TOOLTIP", function() {
@@ -665,6 +665,135 @@ describe("TOOLTIP", function() {
 				expect(eventReceiver.currentIdx).to.be.equal(0);
 			});
 
+		});
+
+		describe("with padding", () => {
+			before(() => {
+				args = {
+					data: {
+						columns: [
+							["data1", 30, 200, 100, 400, 150, 250],
+							["data2", 130, 340, 200, 500, 250, 350]
+						],
+						type: "line"
+					},
+					padding: {
+						left: 400
+					},
+					axis: {
+						rotated: false,
+						y: {
+							label: "y axis label"
+						}
+					}
+				}
+			});
+
+			it("should displayed in correct position with padding.left", () => {
+				const x = 4;
+
+				// when
+				chart.tooltip.show({x});
+
+				const tooltip = chart.$.tooltip.node().getBoundingClientRect();
+				const pointLeft = chart.$.circles.filter(`.${$CIRCLE.circle}-${x}`).node().getBoundingClientRect().left;
+
+				expect(tooltip.left + tooltip.width).to.be.below(pointLeft);
+				expect(tooltip.left + tooltip.width).to.be.closeTo(pointLeft, 15);
+			});
+
+			it("set options: axis.y.label", () => {
+				args.axis.y.label = {
+					text: "y axis label"
+				};
+			});
+
+			it("should displayed in correct position with padding.left", () => {
+				const x = 4;
+
+				// when
+				chart.tooltip.show({x});
+
+				const tooltip = chart.$.tooltip.node().getBoundingClientRect();
+				const pointLeft = chart.$.circles.filter(`.${$CIRCLE.circle}-${x}`).node().getBoundingClientRect().left;
+
+				expect(tooltip.left + tooltip.width).to.be.below(pointLeft);
+				expect(tooltip.left + tooltip.width).to.be.closeTo(pointLeft, 15);
+			});
+
+			it("set options: axis.y.label", () => {
+				args.axis.y.label = {
+					text: "y axis label",
+					position: "inner-middle"
+				};
+			});
+
+			it("should displayed in correct position with padding.left", () => {
+				const x = 4;
+
+				// when
+				chart.tooltip.show({x});
+
+				const tooltip = chart.$.tooltip.node().getBoundingClientRect();
+				const pointLeft = chart.$.circles.filter(`.${$CIRCLE.circle}-${x}`).node().getBoundingClientRect().left;
+
+				expect(tooltip.left + tooltip.width).to.be.below(pointLeft);
+				expect(tooltip.left + tooltip.width).to.be.closeTo(pointLeft, 15);
+			});
+
+			it("set options: axis.rotated=true", () => {
+				args.axis.rotated = true;
+			});
+
+			it("should displayed in correct position with padding.left on rotated axis", () => {
+				const x = 2;
+
+				// when
+				util.hoverChart(chart, "mousemove", {clientX: 0, clientY: 200});
+
+				const tooltip = chart.$.tooltip.node().getBoundingClientRect();
+				const pointLeft = chart.$.circles.filter(`.${$CIRCLE.circle}-${x}`).node().getBoundingClientRect().left;
+
+				expect(pointLeft).to.be.below(tooltip.left + tooltip.width);
+				expect(tooltip.left).to.be.closeTo(pointLeft, 30);
+			});
+
+			it("set options: axis.rotated=true", () => {
+				args.axis.rotated = false;
+				args.padding = {
+					top: 100
+				};
+			});
+
+			it("should displayed in correct position with padding.top", () => {
+				const x = 3;
+
+				// when
+				chart.tooltip.show({x});
+
+				const tooltip = chart.$.tooltip.node().getBoundingClientRect();
+				const pointTop = chart.$.circles.filter(`.${$CIRCLE.circle}-${x}`).node().getBoundingClientRect().top;
+
+				expect(tooltip.top).to.be.below(pointTop);
+				expect(tooltip.top + tooltip.height).to.be.closeTo(pointTop, 30);
+			});
+
+			it("set options: axis.rotated=true", () => {
+				args.axis.rotated = true;
+			});
+
+			it("should displayed in correct position with padding.top on rotated axis", () => {
+				const x = 2;
+
+				// when
+				util.hoverChart(chart, "mousemove", {clientX: 0, clientY: 250});
+
+				const tooltip = chart.$.tooltip.node().getBoundingClientRect();
+				const pointTop = chart.$.circles.filter(`.${$CIRCLE.circle}-${x}`).node().getBoundingClientRect().top;
+
+				expect(pointTop).to.be.below(tooltip.top + tooltip.height);
+				expect(tooltip.top).to.be.closeTo(pointTop, 30);
+			});
 		});
 
 		describe("on rotated axis", () => {

commit 8106e14b7800e73ce570d855ebf9fa5368f325cc

fix(tooltip): Fix tooltip error on Arc type
Side effect caused by #3475.
The condition set where valid only for axis based chart

Ref https://github.com/naver/billboard.js/issues/3473#issuecomment-1781091685

src/ChartInternal/internals/size.ts
@@ -86,16 +86,18 @@ export default {
 
 	getSvgLeft(withoutRecompute?: boolean): number {
 		const $$ = this;
-		const {config, $el} = $$;
+		const {config, state: {hasAxis}, $el} = $$;
 		const isRotated = config.axis_rotated;
 		const hasLeftAxisRect = isRotated || (!isRotated && !config.axis_y_inner);
 		const leftAxisClass = isRotated ? $AXIS.axisX : $AXIS.axisY;
 		const leftAxis = $el.main.select(`.${leftAxisClass}`).node();
-		const leftLabel = config[`axis_${isRotated ? "x" : "y"}_label`];
+		const leftLabel = hasAxis && config[`axis_${isRotated ? "x" : "y"}_label`];
 		let labelWidth = 0;
 
 		// if axis label position set to inner, exclude from the value
-		if (isString(leftLabel) || isString(leftLabel.text) || /^inner-/.test(leftLabel?.position)) {
+		if (hasAxis && (
+			isString(leftLabel) || isString(leftLabel.text) || /^inner-/.test(leftLabel?.position)
+		)) {
 			const label = $el.main.select(`.${leftAxisClass}-label`);
 
 			if (!label.empty()) {

test/esm/donut-spec.ts
@@ -43,4 +43,14 @@ describe("ESM donut", function() {
             done();
         }, 500);        
     });
+
+    it("shouldn't throw error on call of tooltip API.", () => {
+        delete chart.internal.config.axis_y_label;
+
+        expect(
+            chart.tooltip.show({x:0})
+        ).to.not.throw;
+
+        chart.internal.config.axis_y_label = {};
+    });
 });