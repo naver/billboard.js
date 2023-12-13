75624f74e8c78ccc83e6284b5a6df9954b7f030b
netil <alberto.park@gmail.com>
2023-12-13 00:47:40 +0000

    skip: 3.10.3-nightly-20231213004621 build [skip ci]

--- dist/billboard.pkgd.js
+++ dist/billboard.pkgd.js
@@ -5,7 +5,7 @@
  * billboard.js, JavaScript chart library
  * https://naver.github.io/billboard.js/
  *
- * @version 3.10.3-nightly-20231212004620
+ * @version 3.10.3-nightly-20231213004621
  *
  * All-in-one packaged file for ease use of 'billboard.js' with dependant d3.js modules & polyfills.
  * - d3-axis ^3.0.0
@@ -53692,7 +53692,7 @@ let _defaults = {};
 
 /**
  * @namespace bb
- * @version 3.10.3-nightly-20231212004620
+ * @version 3.10.3-nightly-20231213004621
  */
 const bb = {
   /**
@@ -53702,7 +53702,7 @@ const bb = {
    *    bb.version;  // "1.0.0"
    * @memberof bb
    */
-  version: "3.10.3-nightly-20231212004620",
+  version: "3.10.3-nightly-20231213004621",
   /**
    * Generate chart
    * - **NOTE:** Bear in mind for the possiblity of ***throwing an error***, during the generation when:
