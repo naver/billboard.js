945251b3a768944d16fb515cd5331f910a14ecfe
Jae Sung Park <alberto.park@gmail.com>
2024-05-28 15:27:21 +0900

    fix(funnel): Fix padding and unload
    - Fix incorrect path with padding option
    - Fix incorrect path generation on unload
    
    Fix #3787

--- demo/types/types.js
+++ demo/types/types.js
@@ -16,6 +16,7 @@ const Types = {
         "treemap",
         "polar",
         "radar",
+        "funnel",
         "gauge",
         "gauge-multi",
         "gauge-stack-data",
@@ -104,6 +105,16 @@ const Types = {
             bubble: {
                 maxR: 15
             },
+            funnel: {
+                neck: {
+                    width: {
+                        ratio: 0.3
+                    },
+                    height: {
+                        ratio: 0.35
+                    }
+                }
+            },
             gauge: {
                 title: "100%",
                 label: {
@@ -180,6 +191,19 @@ const Types = {
             } else if (type === "grouped-bar") {
                 type = "bar";
                 options.data.groups = [["data0", "data1"]];
+
+            } else if (type === "funnel") {
+                options.data.columns = [
+                    ["data0", 100],
+                    ["data1", 50],
+                    ["data2", 30]
+                ];
+
+                options.padding = {
+                    top: 30,
+                    left: 10,
+                    right: 10
+                };
 
             } else if (type === "gauge") {
                 options.data.columns = [["data0", 70]];
@@ -448,6 +472,10 @@ const Types = {
                     .classed("title", true)
                     .text(v);
             }
+
+        // if (type === "gauge") {
+        //     debugger;
+        // }
 
             bb.generate(options);
         });
