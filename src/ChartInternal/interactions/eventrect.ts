4b9bb93a58ff19d9aa4c443b65b49b273a157a6d
netil <netil@AD02298065.local>
2024-11-15 15:53:58 +0900

    refactor(point): remove duplication getting sensitivity value

--- src/ChartInternal/interactions/eventrect.ts
+++ src/ChartInternal/interactions/eventrect.ts
@@ -671,7 +671,6 @@ export default {
 	clickHandlerForMultipleXS(ctx): void {
 		const $$ = ctx;
 		const {config, state} = $$;
-		const pointSensitivity = config.point_sensitivity;
 		const targetsToShow = $$.filterTargetsToShow($$.data.targets);
 
 		if ($$.hasArcType(targetsToShow)) {
@@ -680,9 +679,7 @@ export default {
 
 		const mouse = getPointer(state.event, this);
 		const closest = $$.findClosestFromTargets(targetsToShow, mouse);
-		const sensitivity = pointSensitivity === "radius" ? closest?.r : (
-			isFunction(pointSensitivity) ? closest && pointSensitivity(closest) : pointSensitivity
-		);
+		const sensitivity = $$.getPointSensitivity(closest);
 
 		if (!closest) {
 			return;
