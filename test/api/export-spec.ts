3fe8c3583ddf01a57039e25a52a359dc6a6eff4f
Jae Sung Park <alberto.park@gmail.com>
2025-02-06 14:39:53 +0900

    chore(deps-dev): update dependency (#3945)
    Co-authored-by: netil <netil@AD02298065.local>

--- test/api/export-spec.ts
+++ test/api/export-spec.ts
@@ -201,9 +201,9 @@ describe("API export", () => {
 
 			// pattern for local: preserveFontStyle=true
 			[
-				"BvTQtGxT72vgDsOwkU2xaEizL1iU8C42iAvjQgANecBNNwSWbPoMwi",
-				"rbn4q78UGcAmAqZn0zhxdVQAsbSz4srQCoD9sUQD0hw8KgP7wQQHQ",
-				"79VQVUAVVAFVAFVIHQK6AAGPohoAKoAqqAKqAKqAKqQNgUUAAMm+PaX1VAFVAFVAFVQ"
+				"UwUQECoIlWYZu6qgABsKvys3LDFCAAGmYQNocKRKQAATAiIVlMehQgAKbHluxJ",
+				"bAOgeAH1QlUcTZOjJOFPV7zN3zDlq/tQUvMb1kYAjNcQLZROAGxBtNTf0oIXkACY",
+				"CZAACZAACZAABSCfARIgARIgARIgARLIMQIUgDkWcHaXBEiABEiABEiABCgA"
 			],
 			
 			// pattern for webdriverio
@@ -229,9 +229,9 @@ describe("API export", () => {
 
 			// pattern for CI: preserveFontStyle=true
 			[
-				"MMcesnMiKAsgzhwTak0AtArgzgC+WwHEfALtR6FRltwPYGMDxAHYHYAA8B",
-				"2HTkHLCWAg4nrEehzQNDS997GQgCNORiBbCPDmWN4MpEACVRGoFYBrKz35t",
-				"gFcBOMOf8rUAftrP9AcCOMkvvxfA8hxSvQJglx51zgZwRQUAMMg5PQTg21nm"
+				"BvTQtGxT72vgDsOwkU2xaEizL1iU8C42iAvjQgANecBNNwSWbPoMwi",
+				"rbn4q78UGcAmAqZn0zhxdVQAsbSz4srQCoD9sUQD0hw8KgP7wQQHQ",
+				"79VQVUAVVAFVAFVIHQK6AAGPohoAKoAqqAKqAKqAKqQNgUUAAMm+PaX1VAFVAFVAFVQ"
 			]
 		];
 
