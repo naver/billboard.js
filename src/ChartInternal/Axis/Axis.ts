naver/billboard.js

commit 7fa445e6fe8d3b9ae3d785fc199611809766067f

feat(axis): Enhance axis.evalTextSize per axis
- Pass axis id to set size per axis
- Fix determining maxTickSize per axis

Fix #4002

src/ChartInternal/Axis/Axis.ts
@@ -9,7 +9,7 @@ import {
 	axisTop as d3AxisTop
 } from "d3-axis";
 import type {AxisType} from "../../../types/types";
-import {$AXIS} from "../../config/classes";
+import {$AXIS, $COMMON} from "../../config/classes";
 import {
 	capitalize,
 	isArray,
@@ -682,14 +682,24 @@ class Axis {
 				.style("visibility", "hidden")
 				.style("position", "fixed")
 				.style("top", "0")
-				.style("left", "0");
+				.style("left", "0")
+				.append("g")
+				.attr("class", `${$AXIS[`axis${capitalize(id)}`]} ${$COMMON.dummy}`);
 
 			axis.create(dummy);
+
+			// when evalTextSize is set as function, sizeFor1Char is set to the dummy element
+			const {sizeFor1Char} = dummy.node();
 
 			dummy.selectAll("text")
 				.attr("transform", isNumber(tickRotate) ? `rotate(${tickRotate})` : null)
 				.each(function(d, i) {
-					const {width, height} = this.getBoundingClientRect();
+					const {width, height} = sizeFor1Char ?
+						{
+							width: this.textContent.length * sizeFor1Char.w,
+							height: sizeFor1Char.h
+						} :
+						this.getBoundingClientRect();
 
 					max.width = Math.max(max.width, width);
 					max.height = Math.max(max.height, height);

demo/demo.js
@@ -1231,6 +1231,51 @@ var demos = {
 				}
 			}
 		},
+		AxisEvalTextSize: [
+			{
+				options: {
+					data: {
+						columns: [
+							["data1", 30, 200, 100, 400, 150, 250],
+							["data2", 50, 20, 10, 40, 15, 25]
+						],
+					},
+					axis: {
+						y2: {
+							show: true
+						}
+					}
+				},
+				style: [
+					"#axisEvalTextSize_1 .bb-axis-x text{font-size: 20px;}",
+					"#AxisEvalTextSize_1 .bb-axis-y text{font-size: 35px;}",
+					"#AxisEvalTextSize_1 .bb-axis-y2 text{font-size: 25px;}"
+				]
+			},
+			{
+				options: {
+					data: {
+						columns: [
+							["data1", 30, 200, 100, 400, 150, 250],
+							["data2", 50, 20, 10, 40, 15, 25]
+						],
+					},
+					axis: {
+						y2: {
+							show: true
+						},
+						evalTextSize: function(text, id) {
+						// specify manual axis text character size
+						return {
+							x: { w: 30, h: 30},
+						    y: { w: 25, h: 20},
+						    y2: { w: 15.5, h: 20}
+						}[id];
+						}
+					}
+				}
+			}
+		],
 		AxisLabel: {
 			options: {
 				data: {

demo/simple-sidebar.css
@@ -432,6 +432,10 @@ div.row {
     border-top: 1px dotted #a7a7a7;
 }
 
+#axisEvalTextSize_1 .bb-axis-x text{font-size: 20px;}
+#axisEvalTextSize_1 .bb-axis-y text{font-size: 35px;}
+#axisEvalTextSize_1 .bb-axis-y2 text{font-size: 25px;}
+
 /* div[id$="_2"], div[id$="_3"], div[id$="_4"], div[id$="_5"] {
     margin: 20px 0;
     box-shadow: 0 -1px 0 #ccc;

src/ChartInternal/Axis/AxisRenderer.ts
@@ -5,6 +5,7 @@
  */
 import {select as d3Select} from "d3-selection";
 import type {d3Selection} from "../../../types/types";
+import {$COMMON} from "../../config/classes";
 import {isArray, isFunction, isNumber, isString, toArray} from "../../module/util";
 import Helper from "./AxisRendererHelper";
 
@@ -139,10 +140,21 @@ export default class AxisRenderer {
 				tickShow.text && tickEnter.append("text");
 
 				const tickText = tick.select("text");
-				const sizeFor1Char = isFunction(evalTextSize) ?
-					evalTextSize.bind(ctx.params.owner.api)(tickText.node()) :
-					Helper.getSizeFor1Char(tickText, evalTextSize);
 				const counts: number[] = [];
+				let sizeFor1Char: {w: number, h: number} | undefined;
+
+				if (isFunction(evalTextSize)) {
+					// set evalTextSize to dummy axis element to be used in .getMaxTickSize()
+					sizeFor1Char = evalTextSize.bind(ctx.params.owner.api)(tickText.node(), id);
+
+					if (this.classList.contains($COMMON.dummy)) {
+						this.sizeFor1Char = sizeFor1Char;
+					}
+				}
+
+				if (!sizeFor1Char) {
+					sizeFor1Char = Helper.getSizeFor1Char(tickText, !!evalTextSize);
+				}
 
 				let tspan: d3Selection = tickText
 					.selectAll("tspan")

src/ChartInternal/data/data.ts
@@ -300,9 +300,12 @@ export default {
 			let min = [];
 			let max = [];
 
+			// Cache the getFilteredDataByValue function calls
+			const {min: minVal, max: maxVal} = minMax;
+
 			data.forEach(v => {
-				const minData = $$.getFilteredDataByValue(v, minMax.min);
-				const maxData = $$.getFilteredDataByValue(v, minMax.max);
+				const minData = $$.getFilteredDataByValue(v, minVal);
+				const maxData = $$.getFilteredDataByValue(v, maxVal);
 
 				if (minData.length) {
 					min = min.concat(minData);
@@ -582,20 +585,8 @@ export default {
 	},
 
 	checkValueInTargets(targets, checker: Function): boolean {
-		const ids = Object.keys(targets);
-		let values;
-
-		for (let i = 0; i < ids.length; i++) {
-			values = targets[ids[i]].values;
-
-			for (let j = 0; j < values.length; j++) {
-				if (checker(values[j].value)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
+		return Object.keys(targets)
+			.some(id => targets[id].values.some(v => checker(v.value)));
 	},
 
 	hasMultiTargets(): boolean {

src/config/Options/axis/axis.ts
@@ -18,27 +18,41 @@ export default {
 	 * @memberof Options
 	 * @type {boolean|Function}
 	 * @default true
+	 * @see [Demo](https://naver.github.io/billboard.js/demo/#Axis.AxisEvalTextSize)
 	 * @example
 	 * axis: {
 	 *   // will evaluate getting text size every time.
 	 *   evalTextSize: false.
 	 *
 	 *   // set a custom evaluator
-	 *   evalTextSize: function(textElement) {
+	 *   evalTextSize: function(textElement, axisId) {
 	 *     // set some character to be evaluated
-	 *     text.textContent = "0";
+	 *     // NOTE: The dummy textElement is a descendant of given axisId('x', 'y' or 'y2').
+	 *     textElement.textContent = "0";
 	 *
 	 *     // get the size
-	 *      const box = text.getBBox();
+	 *     const box = textElement.getBBox();
 	 *
 	 *     // clear text
-	 *     text.textContent = "";
+	 *     textElement.textContent = "";
 	 *
 	 *     return { w: 7, h: 12};
+	 *   },
+	 *
+	 *   // set a custom evaluator by returning fixed value
+	 *   evalTextSize: function(textElement, axisId) {
+	 *     return {
+	 *        x: {w: 7, h: 12},
+	 *        y: {w: 15.75, h: 30},
+	 *        y2: {w: 9.5, h: 18}
+	 *     }[axisId];
 	 *   }
 	 * }
 	 */
-	axis_evalTextSize: <boolean | ((text: SVGTextElement) => {w: number, h: number})>true,
+	axis_evalTextSize: <
+		| boolean
+		| ((text: SVGTextElement, axisId: "x" | "y" | "y2") => {w: number, h: number})
+	>true,
 
 	/**
 	 * Switch x and y axis position.

src/config/classes.ts
@@ -12,7 +12,8 @@ export const $COMMON = {
 	empty: "bb-empty",
 	main: "bb-main",
 	target: "bb-target",
-	EXPANDED: "_expanded_"
+	EXPANDED: "_expanded_",
+	dummy: "_dummy_"
 };
 
 export const $ARC = {

test/assets/common.css
@@ -15,3 +15,15 @@
 .regions_class1{
     fill: red;
 }
+
+.eval-text-size .bb-axis-x text {
+	font-size: 20px;
+}
+
+.eval-text-size .bb-axis-y text {
+	font-size: 35px;
+}
+
+.eval-text-size .bb-axis-y2 text {
+	font-size: 30px;
+}

commit 20487661bd61c3737f319f1f7eef7159ae0da076

refactor(axis): update dummy element handling and improve axis rendering
- Fix side-effect caused from #4003
- HW Accelerate tooltip layer to reduce paintings

src/ChartInternal/Axis/Axis.ts
@@ -683,14 +683,16 @@ class Axis {
 				.style("visibility", "hidden")
 				.style("position", "fixed")
 				.style("top", "0")
-				.style("left", "0")
+				.style("left", "0");
+
+			const g = dummy
 				.append("g")
 				.attr("class", `${$AXIS[`axis${capitalize(id)}`]} ${$COMMON.dummy}`);
 
-			axis.create(dummy);
+			axis.create(g);
 
 			// when evalTextSize is set as function, sizeFor1Char is set to the dummy element
-			const {sizeFor1Char} = dummy.node();
+			const {sizeFor1Char} = g.node();
 
 			dummy.selectAll("text")
 				.attr("transform", isNumber(tickRotate) ? `rotate(${tickRotate})` : null)

src/scss/billboard.scss
@@ -203,6 +203,7 @@
 .bb-tooltip-container {
 	z-index: 10;
 	user-select: none;
+	transform: translateZ(0);
 }
 
 .bb-tooltip {

src/scss/theme/dark.scss
@@ -244,6 +244,7 @@ rect.bb-circle, use.bb-circle {
     z-index: 10;
     font-family: $font-family;
 	user-select: none;
+    transform: translateZ(0);
 }
 
 .bb-tooltip {

src/scss/theme/datalab.scss
@@ -226,6 +226,7 @@ $text-font-size: 11px;
     z-index: 10;
     font-family: $font-family;
 	user-select: none;
+    transform: translateZ(0);
 }
 
 .bb-tooltip {

src/scss/theme/graph.scss
@@ -235,6 +235,7 @@ rect.bb-circle, use.bb-circle {
     z-index: 10;
     font-family: $font-family;
 	user-select: none;
+    transform: translateZ(0);
 }
 
 .bb-tooltip {

src/scss/theme/insight.scss
@@ -227,6 +227,7 @@ rect.bb-circle, use.bb-circle {
     z-index: 10;
     font-family: $font-family;
 	user-select: none;
+    transform: translateZ(0);
 }
 
 .bb-tooltip {

src/scss/theme/modern.scss
@@ -245,6 +245,7 @@ rect.bb-circle, use.bb-circle {
     z-index: 10;
     font-family: $font-family;
 	user-select: none;
+    transform: translateZ(0);
 }
 
 .bb-tooltip {